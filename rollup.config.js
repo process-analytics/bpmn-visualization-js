494a8df77b29b9a2915926a14c939c6ad557b4a6
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-03-02 13:34:04 +0100

    Build, Start scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,33 @@
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
-import copy from 'rollup-plugin-copy'
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import copy from 'rollup-plugin-copy';
 
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+let devMode = process.env.devMode;
+let plugins = [
+  typescript({
+    typescript: require('typescript'),
+  })];
+
+if (devMode) {
+  // Create a server for dev mode
+  plugins.push(serve('dist'));
+  // Allow to livereload on any update
+  plugins.push(livereload({watch: 'dist', verbose: true}));
+  // Copy index.html to dist
+  plugins.push(copy({targets: [{src: 'src/index.html', dest: 'dist/'}]}));
+}
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: pkg.module,
-      format: 'es',
+      format: 'es'
     },
   ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ], plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-    serve('dist'),      // index.html should be in root of project
-    livereload({
-      watch: 'dist',
-      verbose: true
-    }),
-    copy({
-      targets: [
-        { src: 'src/index.html', dest: 'dist/' }
-      ]
-    })
-  ],
-}
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: plugins
+};
