684f1ebcf397e04fb7c93ef76e2168a327097ca4
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-05 17:32:36 +0100

    Change the coordinates & size of shapes in mxgraph with the bounds of the internal model

--- src/component/mxgraph/MxGraphConverter.ts
+++ src/component/mxgraph/MxGraphConverter.ts
@@ -6,11 +6,11 @@ export default class MxGraphConverter {
 
   public insertShapes(shapes: Shape[]): void {
     shapes.forEach(shape => {
-      //const bounds = shape.bounds;
+      const bounds = shape.bounds;
       const bpmnElement = shape.bpmnElement;
       if (bpmnElement !== undefined && bpmnElement !== null) {
-        //this.graph.insertVertex(lane, bpmnElement.id, bpmnElement.name, bounds.x, bounds.y, bounds.width, bounds.height, bpmnElement.kind);
-        this.graph.insertVertex(this.graph.getDefaultParent(), bpmnElement.id, bpmnElement.name, 0, 0, 20, 20, bpmnElement.kind);
+        const parent = this.graph.getDefaultParent();
+        this.graph.insertVertex(parent, bpmnElement.id, bpmnElement.name, bounds.x, bounds.y, bounds.width, bounds.height, bpmnElement.kind);
       }
     });
   }
