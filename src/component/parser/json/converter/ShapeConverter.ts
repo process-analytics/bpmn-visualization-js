106f64aee48974038fab0461f577675fd27d883f
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-11 16:43:01 +0100

    manage shapes (non lanes) array

--- src/component/parser/json/converter/ShapeConverter.ts
+++ src/component/parser/json/converter/ShapeConverter.ts
@@ -18,17 +18,24 @@ export class ShapeConverter extends AbstractConverter<Shape[]> {
   deserialize(bpmnDiagram: Array<any> | any): Shape[] {
     try {
       const shapes = bpmnDiagram.BPMNPlane.BPMNShape;
-      const bpmnElement = shapes.bpmnElement;
-      const shapeBpmnElement = findShapeBpmnElement(bpmnElement);
+      return (
+        ensureIsArray(shapes)
+          //.filter(shape => )
+          .map(shape => {
+            const bpmnElement = shape.bpmnElement;
+            const shapeBpmnElement = findShapeBpmnElement(bpmnElement);
 
-      if (shapeBpmnElement) {
-        const id = shapes.id;
-        const bounds3 = shapes.Bounds;
-        const bounds2 = jsonConvert.deserializeObject(bounds3, Bounds);
+            if (shapeBpmnElement) {
+              const id = shape.id;
+              const bounds3 = shape.Bounds;
+              const bounds2 = jsonConvert.deserializeObject(bounds3, Bounds);
 
-        return [new Shape(id, shapeBpmnElement, bounds2)];
-      }
-      return [];
+              return new Shape(id, shapeBpmnElement, bounds2);
+            }
+          })
+          // TODO manage this in another way
+          .filter(shape => shape)
+      );
     } catch (e) {
       // TODO error management
       console.log(e as Error);
