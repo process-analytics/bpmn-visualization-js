f7c81973f90b920f9b391b94662c227d75c42a39
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-13 10:20:52 +0100

    Split test files for lane

---
+++ test/component/parser/json/JsonTestUtils.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import { ShapeBpmnElementKind } from '../../../../src/model/bpmn/shape/ShapeBpmnElementKind';
+import Shape from '../../../../src/model/bpmn/shape/Shape';
+
+export interface ExpectedShape {
+  shapeId: string;
+  bpmnElementId: string;
+  bpmnElementName: string;
+  bpmnElementKind: ShapeBpmnElementKind;
+  boundsX: number;
+  boundsY: number;
+  boundsWidth: number;
+  boundsHeight: number;
+}
+
+export function verifyShape(shape: Shape, expectedValue: ExpectedShape): void {
+  expect(shape.id).to.be.equal(expectedValue.shapeId, 'shape id');
+
+  const bpmnElement = shape.bpmnElement;
+  expect(bpmnElement.id).to.be.equal(expectedValue.bpmnElementId, 'bpmn element id');
+  expect(bpmnElement.name).to.be.equal(expectedValue.bpmnElementName, 'bpmn element name');
+  expect(bpmnElement.kind).to.be.equal(expectedValue.bpmnElementKind, 'bpmn element kind');
+
+  const bounds = shape.bounds;
+  expect(bounds.x).to.be.equal(expectedValue.boundsX, 'bounds x');
+  expect(bounds.y).to.be.equal(expectedValue.boundsY, 'bounds y');
+  expect(bounds.width).to.be.equal(expectedValue.boundsWidth, 'bounds width');
+  expect(bounds.height).to.be.equal(expectedValue.boundsHeight, 'bounds height');
+}
---
+++ test/component/parser/xml/XMLTestUtils.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyProperties(object: any, propertiesToHave: string[], propertiesNotToHave: string[]): void {
+  expect(object).to.be.a('object');
+  propertiesToHave.map(property => expect(object).to.have.property(property)); // TODO msg on failure
+  propertiesNotToHave.map(property => expect(object).not.to.have.property(property)); // TODO msg on failure
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyIsNotEmptyArray(object: any, message: string): void {
+  expect(object).to.be.a('array');
+  expect(object).to.have.length.greaterThan(1, message);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyBounds(shape: any, x: number, y: number, width: number, height: number): void {
+  const bounds = shape.Bounds;
+  expect(bounds.x).eq(x);
+  expect(bounds.y).eq(y);
+  expect(bounds.width).eq(width);
+  expect(bounds.height).eq(height);
+}
+
+export function verifyDefinitions(json: any) {
+  verifyProperties(json, ['definitions'], []);
+  verifyProperties(json.definitions, ['process', 'BPMNDiagram'], []);
+}
