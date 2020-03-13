c96219bab58b076e847f31f6dbc862a01ee72ea7
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-13 11:30:55 +0100

    Change the returned type of BPMN Json parser

--- src/component/parser/json/BpmnJsonParser.ts
+++ src/component/parser/json/BpmnJsonParser.ts
@@ -2,16 +2,11 @@ import Shape from '../../../model/bpmn/shape/Shape';
 import { JsonConvert, OperationMode } from 'json2typescript';
 import { Definitions } from './Definitions';
 import Edge from '../../../model/bpmn/edge/Edge';
+import BpmnModel from '../../../model/bpmn/BpmnModel';
 
 export default class BpmnJsonParser {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public static parse(
-    json: any,
-  ): {
-    lanes: Shape[];
-    flowNodes: Shape[];
-    edges: Edge[];
-  } {
+  public static parse(json: any): BpmnModel {
     const jsonConvert: JsonConvert = new JsonConvert();
     jsonConvert.operationMode = OperationMode.ENABLE;
     const definitions = jsonConvert.deserializeObject(json.definitions, Definitions);
