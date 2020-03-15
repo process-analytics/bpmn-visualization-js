956e57d4b3d030e151f2cacf3365a2a66e5d2e12
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-03-15 08:13:24 +0100

    add doc to ShapeBpmnElementKind

--- src/model/bpmn/shape/ShapeBpmnElementKind.ts
+++ src/model/bpmn/shape/ShapeBpmnElementKind.ts
@@ -1,3 +1,9 @@
+/**
+ * Describes kind of Bpmn elements.
+ *
+ * Values are the same as in the BPMN MODEL namespace (http://www.omg.org/spec/BPMN/20100524/MODEL) to allow automatic
+ * detection of elements in the BPMN source.
+ */
 export enum ShapeBpmnElementKind {
   LANE = 'lane',
   TASK_USER = 'user',
