85fc2bca24603d44855b92c379a2f38e810f8020
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-03-16 17:20:41 +0100

    Refactor: detection based on ShapeBpmnElementKind enum only (#103)
    This change will simplify work to do to support new BPMN element types.
    Previously, we had to modify the `ShapeModelConverter` in addition to the `ShapeBpmnElementKind` enum. The enum change is mandatory because this is how we identify the element for rendering.
    But having to change the converter was a pain and led to code duplication.
    
    The converter now generically considers all elements of the enum to apply the conversion. The BPMN containers (lane for instance) are still managed manually as they require a dedicated processing.
    
    Also introduce a new doc which explains briefly how to support a new BPMN element type.

--- src/model/bpmn/shape/ShapeBpmnElementKind.ts
+++ src/model/bpmn/shape/ShapeBpmnElementKind.ts
@@ -1,5 +1,11 @@
+/**
+ * Describes kind of Bpmn elements.
+ *
+ * Values are the same as in the BPMN MODEL namespace (http://www.omg.org/spec/BPMN/20100524/MODEL) to allow automatic
+ * detection of elements in the BPMN source.
+ */
 export enum ShapeBpmnElementKind {
   LANE = 'lane',
-  TASK_USER = 'user',
-  EVENT_START = 'start',
+  TASK_USER = 'userTask',
+  EVENT_START = 'startEvent',
 }
