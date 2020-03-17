4f06e1683a96cb04620793af8f357c9537a62c47
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-03-17 23:01:04 +0100

    jest replaced mocha, separate config e2e and unit

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  preset: 'jest-puppeteer',
-  roots: ['<rootDir>/test/e2e/'],
+  roots: ['<rootDir>/test/unit/'],
   testMatch: ['**/?(*.)+(spec|test).[t]s'],
-  testPathIgnorePatterns: ['/node_modules/', 'dist'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  testEnvironment: 'jest-environment-puppeteer-jsdom',
-  globalSetup: 'jest-environment-puppeteer-jsdom/setup',
-  globalTeardown: 'jest-environment-puppeteer-jsdom/teardown',
 };
