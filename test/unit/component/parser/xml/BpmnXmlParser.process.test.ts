eb24055851889d613a0dc24b6d22b2c4c46d3e48
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-03-18 12:36:30 +0100

    xml tests with multiple participants

--- test/unit/component/parser/xml/BpmnXmlParser.process.test.ts
+++ test/unit/component/parser/xml/BpmnXmlParser.process.test.ts
@@ -2,6 +2,7 @@ import BpmnXmlParser from '../../../../../src/component/parser/xml/BpmnXmlParser
 import { verifyBounds, verifyDefinitions, verifyDefinitionsWithCollaboration, verifyIsNotEmptyArray, verifyProperties } from './XMLTestUtils';
 import { expect } from 'chai';
 
+// TODO rename into verifyIsNotArray and move to utils
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function expectNotToBeAnArray(obj: any, message?: string): void {
   expect(obj).to.be.a('object', message);
@@ -44,10 +45,7 @@ describe('parse bpmn as xml for process', () => {
     verifyProperties(plane, ['id', 'bpmnElement'], []);
   });
 
-  // TODO here name set in both participant and process
-  // add test when name only available in process
-  // TODO add test with several participants
-  // TODO case when no lane
+  // TODO here name set in both participant and process / add test when name only available in process
   it('bpmn with single process and participant', () => {
     const singleProcess = `<?xml version="1.0" encoding="UTF-8"?>
 <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" targetNamespace="http://bonitasoft.com/_RLk98HH_Eei9Z4IY4QeFuA">
@@ -107,55 +105,63 @@ describe('parse bpmn as xml for process', () => {
     verifyBounds(processShape, 158, 50, 1620, 430);
   });
 
-  //   it('bpmn with multiple processes, ensure lanes are present', () => {
-  //     const processes = `<?xml version="1.0" encoding="UTF-8"?>
-  // <bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" targetNamespace="http://bonitasoft.com/_RLk98HH_Eei9Z4IY4QeFuA">
-  //   <bpmn:process id="Process_0wpi11d" name="RequestLoan" isExecutable="false">
-  //     <bpmn:laneSet id="LaneSet_1i59xiy">
-  //       <bpmn:lane id="Lane_164yevk" name="Customer">
-  //         <bpmn:flowNodeRef>_RLk983H_Eei9Z4IY4QeFuA</bpmn:flowNodeRef>
-  //       </bpmn:lane>
-  //       <bpmn:lane id="Lane_1h5yeu4" />
-  //     </bpmn:laneSet>
-  //   </bpmn:process>
-  //   <bpmn:process id="Process2" name="Notify" isExecutable="false">
-  //     <bpmn:laneSet id="LaneSet2">
-  //       <bpmn:lane id="Lane_12u5n6x" />
-  //     </bpmn:laneSet>
-  //   </bpmn:process>
-  //   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-  //     <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Collaboration_186ohra">
-  //       <bpmndi:BPMNShape id="Lane_164yevk_di" bpmnElement="Lane_164yevk" isHorizontal="true">
-  //         <dc:Bounds x="190" y="110" width="1230" height="83" />
-  //       </bpmndi:BPMNShape>
-  //       <bpmndi:BPMNShape id="Lane_1h5yeu4_di" bpmnElement="Lane_1h5yeu4" isHorizontal="true">
-  //         <dc:Bounds x="190" y="193" width="1230" height="83" />
-  //       </bpmndi:BPMNShape>
-  //       <bpmndi:BPMNShape id="Lane_12u5n6x_di" bpmnElement="Lane_12u5n6x" isHorizontal="true">
-  //         <dc:Bounds x="290" y="210" width="1232" height="252" />
-  //       </bpmndi:BPMNShape>
-  //     </bpmndi:BPMNPlane>
-  //   </bpmndi:BPMNDiagram>
-  // </bpmn:definitions>`;
-  //
-  //     const json = new BpmnXmlParser().parse(processes);
-  //
-  //     verifyDefinitions(json);
-  //
-  //     // Model
-  //     const process = json.definitions.process;
-  //     verifyIsNotEmptyArray(process, 'process');
-  //     const process0 = process[0];
-  //     verifyProperties(process0, ['laneSet'], []);
-  //     verifyProperties(process0.laneSet, ['lane'], []);
-  //     const lanes = process0.laneSet.lane;
-  //     verifyIsNotEmptyArray(lanes, 'lane');
-  //     verifyProperties(lanes[0], ['id', 'name', 'flowNodeRef'], []);
-  //     verifyProperties(lanes[1], ['id'], ['name', 'flowNodeRef']);
-  //
-  //     const process1 = process[1];
-  //     verifyProperties(process1, ['laneSet'], []);
-  //     verifyProperties(process1.laneSet, ['lane'], []);
-  //     verifyProperties(process1.laneSet.lane, ['id'], ['name', 'flowNodeRef']);
-  //   });
+  it('bpmn with multiple processes and participants', () => {
+    const processes = `<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" targetNamespace="http://bonitasoft.com/_RLk98HH_Eei9Z4IY4QeFuA">
+  <bpmn:collaboration id="Collaboration_1">
+    <bpmn:participant id="Participant_1" name="Pool 1" processRef="Process_1" />
+    <bpmn:participant id="Participant_2" name="Pool 2" processRef="Process_2" />
+  </bpmn:collaboration>
+  <bpmn:process id="Process_1" name="RequestLoan" isExecutable="false">
+    <bpmn:laneSet id="LaneSet_1i59xiy">
+      <bpmn:lane id="Lane_164yevk" name="Customer">
+        <bpmn:flowNodeRef>_RLk983H_Eei9Z4IY4QeFuA</bpmn:flowNodeRef>
+      </bpmn:lane>
+      <bpmn:lane id="Lane_1h5yeu4" />
+    </bpmn:laneSet>
+  </bpmn:process>
+  <bpmn:process id="Process_2" name="Notify" isExecutable="false">
+    <bpmn:laneSet id="LaneSet2">
+      <bpmn:lane id="Lane_12u5n6x" />
+    </bpmn:laneSet>
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Collaboration_186ohra">
+      <bpmndi:BPMNShape id="Participant_1_di" bpmnElement="Participant_1" isHorizontal="true">
+        <dc:Bounds x="158" y="50" width="1620" height="430" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Participant_2_di" bpmnElement="Participant_2" isHorizontal="true">
+        <dc:Bounds x="158" y="450" width="1620" height="430" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Lane_164yevk_di" bpmnElement="Lane_164yevk" isHorizontal="true">
+        <dc:Bounds x="190" y="110" width="1230" height="83" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Lane_1h5yeu4_di" bpmnElement="Lane_1h5yeu4" isHorizontal="true">
+        <dc:Bounds x="190" y="193" width="1230" height="83" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="Lane_12u5n6x_di" bpmnElement="Lane_12u5n6x" isHorizontal="true">
+        <dc:Bounds x="290" y="210" width="1232" height="252" />
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>`;
+
+    const json = new BpmnXmlParser().parse(processes);
+
+    verifyDefinitionsWithCollaboration(json);
+
+    // Collaboration
+    const participant = json.definitions.collaboration.participant;
+    verifyIsNotEmptyArray(participant, 'participant');
+
+    expect(participant[0].id).eq('Participant_1', 'participant[0] id');
+    expect(participant[1].id).eq('Participant_2', 'participant[1] id');
+
+    // BPMNDiagram
+    const shapes = verifyAndGetBPMNShape(json);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const processShapes = (shapes as Array<any>).filter(shape => (shape.id as string).startsWith('Participant_'));
+    verifyIsNotEmptyArray(processShapes, 'process shapes');
+    processShapes.forEach(shape => verifyProperties(shape, ['id', 'bpmnElement', 'Bounds'], []));
+  });
 });
