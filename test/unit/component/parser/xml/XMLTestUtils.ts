137dede37328324bda96c256b45ba504b99c61af
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-03-19 01:08:20 +0100

    xml test: improve declaration of a function returned type

--- test/unit/component/parser/xml/XMLTestUtils.ts
+++ test/unit/component/parser/xml/XMLTestUtils.ts
@@ -39,7 +39,7 @@ export function verifyDefinitionsWithCollaboration(json: any): void {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function verifyAndGetBPMNShape(json: any): any {
+export function verifyAndGetBPMNShape(json: any): any | Array<any> {
   const diagram = json.definitions.BPMNDiagram;
   verifyProperties(diagram, ['BPMNPlane'], []);
   const plane = diagram.BPMNPlane;
