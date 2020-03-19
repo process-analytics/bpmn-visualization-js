c4c4fed7101584c08f676659c350605565cee4d1
Céline Souchet <celine.souchet@bonitasoft.com>
2020-03-19 12:11:39 +0100

    Add style for End event

--- src/component/mxgraph/MxGraphConfigurator.ts
+++ src/component/mxgraph/MxGraphConfigurator.ts
@@ -19,6 +19,7 @@ export default class MxGraphConfigurator {
     this.configureDefaultVertexStyle();
     this.configureLaneStyle();
     this.configureStartEventStyle();
+    this.configureEndEventStyle();
     this.configureUserTaskStyle();
     this.configureTaskStyle();
     this.configureParallelGatewayStyle();
@@ -71,6 +72,13 @@ export default class MxGraphConfigurator {
     style[this.mxConstants.STYLE_STROKEWIDTH] = 1.7;
     style[this.mxConstants.STYLE_GRADIENTCOLOR] = '#E9ECB1';
     this.putCellStyle(ShapeBpmnElementKind.EVENT_START, style);
+  }
+
+  private configureEndEventStyle(): void {
+    const style = this.mxUtils.clone(this.getStylesheet().getCellStyle(ShapeBpmnElementKind.EVENT_START), this.getDefaultVertexStyle());
+    style[this.mxConstants.STYLE_STROKECOLOR] = '#8A151A';
+    style[this.mxConstants.STYLE_GRADIENTCOLOR] = 'Crimson';
+    this.putCellStyle(ShapeBpmnElementKind.EVENT_END, style);
   }
 
   private configureUserTaskStyle(): void {
