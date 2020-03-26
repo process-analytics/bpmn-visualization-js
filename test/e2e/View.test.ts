5f289ab9ebd770d395eb05c2edb6e5d062edff17
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2020-03-26 16:54:47 +0100

    Add e2e test to verify cell coordinates relative to its parent

--- test/e2e/View.test.ts
+++ test/e2e/View.test.ts
@@ -144,4 +144,46 @@ describe('BPMN Visu JS', () => {
     // rendering - not OK - when graph is being initialized the window.document.getElementById('graph') is null
     // await expect(page.waitForSelector('[data-cell-id="startEvent_1"]')).resolves.toBeDefined();
   });
+
+  it('Bpmn shape should have coordinates relative to its parent', async () => {
+    const simpleBpmn = `<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_1" targetNamespace="http://example.com/schema/bpmn">
+  <bpmn:collaboration id="Collaboration_1">
+    <bpmn:participant id="Participant_1" name="Process" processRef="Process_1" />
+  </bpmn:collaboration>
+  <bpmn:process id="Process_1" isExecutable="false">
+    <bpmn:startEvent id="StartEvent_1" name="start">
+    </bpmn:startEvent>
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Collaboration_1">
+      <bpmndi:BPMNShape id="BPMNShape_Participant_1" bpmnElement="Participant_1" isHorizontal="true">
+        <dc:Bounds x="100" y="20" width="900" height="180" />
+      </bpmndi:BPMNShape>
+      <bpmndi:BPMNShape id="BPMNShape_StartEvent_1" bpmnElement="StartEvent_1">
+        <dc:Bounds x="250" y="100" width="40" height="40" />
+        <bpmndi:BPMNLabel>
+          <dc:Bounds x="260" y="60" width="30" height="20" />
+        </bpmndi:BPMNLabel>
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>
+`;
+    graph.load(simpleBpmn);
+
+    // TODO use expectModelContainsCell when 'start event' will use a custom shape + make this function returns the cell for adding extra expect
+    const cellId = 'StartEvent_1';
+    const cell = graph.graph.model.getCell(cellId);
+    expect(cell).not.toBeNull();
+
+    const geometry = cell.getGeometry();
+    // absolute coordinates: parent 100, cell 250
+    expect(geometry.x).toEqual(150);
+    // absolute coordinates: parent 20, cell 100
+    expect(geometry.y).toEqual(80);
+    // unchanged as no transformation on size
+    expect(geometry.width).toEqual(40);
+    expect(geometry.height).toEqual(40);
+  });
 });
