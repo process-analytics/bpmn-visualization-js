41d7628ec2ed6359aeb7bc5b7f2ddb9a47b587e7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-01 08:56:57 +0200

    wip: tests to 1st reproduce current behaviour

--- test/unit/component/parser/json/BpmnJsonParser.startEvent.test.ts
+++ test/unit/component/parser/json/BpmnJsonParser.startEvent.test.ts
@@ -146,4 +146,179 @@ describe('parse bpmn as json for start event', () => {
       boundsHeight: 46,
     });
   });
+
+  // TODO to implement
+  it('json containing one process with an array of start events which are not NONE event', () => {
+    /*
+
+    {
+      "definitions": {
+        "id": "Definitions_0185lyd",
+        "targetNamespace": "http://modeler.example.com/schema/bpmn",
+        "process": {
+          "id": "Process_1",
+          "isExecutable": false,
+          "startEvent": {
+            "id": "StartEvent_1",
+            "name": "message start event",
+            "outgoing": "Flow_1",
+            "messageEventDefinition": {
+              "id": "MessageEventDefinition_1"
+            }
+          },
+          "task": {
+            "id": "Task_1",
+            "name": "task1",
+            "incoming": "Flow_1",
+            "outgoing": "Flow_2"
+          },
+          "sequenceFlow": [
+            {
+              "id": "Flow_1",
+              "sourceRef": "StartEvent_1",
+              "targetRef": "Task_1"
+            },
+            {
+              "id": "Flow_2",
+              "sourceRef": "Task_1",
+              "targetRef": "EndEvent_1"
+            }
+          ],
+          "endEvent": {
+            "id": "EndEvent_1",
+            "name": "message end event",
+            "incoming": "Flow_2",
+            "messageEventDefinition": {
+              "id": "MessageEventDefinition_2"
+            }
+          }
+        },
+        "BPMNDiagram": {
+          "id": "BPMNDiagram_1",
+          "BPMNPlane": {
+            "id": "BPMNPlane_1",
+            "bpmnElement": "Process_1",
+            "BPMNEdge": [
+              {
+                "id": "edge_Flow_1",
+                "bpmnElement": "Flow_1",
+                "waypoint": [
+                  {
+                    "x": 192,
+                    "y": 149
+                  },
+                  {
+                    "x": 290,
+                    "y": 149
+                  }
+                ]
+              },
+              {
+                "id": "edge_Flow_2",
+                "bpmnElement": "Flow_2",
+                "waypoint": [
+                  {
+                    "x": 390,
+                    "y": 149
+                  },
+                  {
+                    "x": 492,
+                    "y": 149
+                  }
+                ]
+              }
+            ],
+            "BPMNShape": [
+              {
+                "id": "shape_StartEvent_1",
+                "bpmnElement": "StartEvent_1",
+                "Bounds": {
+                  "x": 156,
+                  "y": 131,
+                  "width": 36,
+                  "height": 36
+                },
+                "BPMNLabel": {
+                  "Bounds": {
+                    "x": 138,
+                    "y": 86,
+                    "width": 71,
+                    "height": 27
+                  }
+                }
+              },
+              {
+                "id": "Task_1_di",
+                "bpmnElement": "Task_1",
+                "Bounds": {
+                  "x": 290,
+                  "y": 109,
+                  "width": 100,
+                  "height": 80
+                }
+              },
+              {
+                "id": "Event_1by852b_di",
+                "bpmnElement": "EndEvent_1",
+                "Bounds": {
+                  "x": 492,
+                  "y": 131,
+                  "width": 36,
+                  "height": 36
+                },
+                "BPMNLabel": {
+                  "Bounds": {
+                    "x": 477,
+                    "y": 174,
+                    "width": 67,
+                    "height": 27
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+
+    */
+
+    const json = `{
+                "definitions" : {
+                    "process": {
+                        "startEvent": [
+                          {
+                              "id":"event_id_0",
+                              "name":"event name"
+                          }, {
+                              "id":"event_id_1"
+                          }
+                        ]
+                    },
+                    "BPMNDiagram": {
+                        "name":"process 0",
+                        "BPMNPlane": {
+                            "BPMNShape": [
+                              {
+                                "id":"shape_startEvent_id_0",
+                                "bpmnElement":"event_id_0",
+                                "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
+                              }, {
+                                "id":"shape_startEvent_id_1",
+                                "bpmnElement":"event_id_1",
+                                "Bounds": { "x": 365, "y": 235, "width": 35, "height": 46 }
+                              }
+                            ]
+                        }
+                    }
+                }
+            }`;
+
+    const model = parseJsonAndExpectOnlyFlowNodes(json, 2);
+
+    // TODO rename EVENT_START into EVENT_START_NONE for clarity?
+    const noneStartEvents = model.flowNodes.filter(shape => shape.bpmnElement.kind == ShapeBpmnElementKind.EVENT_START);
+    expect(noneStartEvents).toHaveLength(0);
+  });
 });
