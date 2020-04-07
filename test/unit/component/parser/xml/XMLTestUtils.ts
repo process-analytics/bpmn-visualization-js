c2cbd57d0d28d52be1b91e1f14ce2b59efdf8591
Céline Souchet <celine.souchet@bonitasoft.com>
2020-04-07 18:30:22 +0200

    WIP: Update XML parser tests

--- test/unit/component/parser/xml/XMLTestUtils.ts
+++ test/unit/component/parser/xml/XMLTestUtils.ts
@@ -15,7 +15,7 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function verifyProperties(object: any, propertiesToHave: string[], propertiesNotToHave: string[]): void {
+export function verifyProperties(object: any, propertiesToHave: string[], propertiesNotToHave: string[] = []): void {
   expect(typeof object === 'object').toBeTruthy();
   propertiesToHave.map(property => expect(object).toHaveProperty(property)); // TODO msg on failure
   propertiesNotToHave.map(property => expect(object).not.toHaveProperty(property)); // TODO msg on failure
@@ -28,9 +28,9 @@ export function verifyPropertiesValues(object: any, properties: Map<string, stri
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function verifyIsNotEmptyArray(object: any): void {
+export function verifyIsNotEmptyArray(object: any, length = 2): void {
   expect(Array.isArray(object)).toBeTruthy();
-  expect(object.length).toBeGreaterThan(1);
+  expect(object.length).toBeGreaterThanOrEqual(length);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -68,4 +68,117 @@ export function verifyAndGetBPMNShape(json: any): Array<any> {
   const shapes = plane.BPMNShape;
   verifyIsNotEmptyArray(shapes);
   return shapes;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyShapes(json: any, expected: number) {
+  const diagram = json.definitions.BPMNDiagram;
+  verifyIsNotEmptyArray(diagram.BPMNPlane.BPMNShape, expected);
+
+  const shape = diagram.BPMNPlane.BPMNShape[0];
+  verifyProperties(shape, ['id', 'bpmnElement', 'Bounds', 'BPMNLabel']);
+  verifyProperties(shape.Bounds, ['height', 'width', 'x', 'y']);
+  verifyProperties(shape.BPMNLabel, ['id', 'labelStyle', 'Bounds']);
+  verifyProperties(shape.BPMNLabel.Bounds, ['height', 'width', 'x', 'y']);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyPlane(json: any) {
+  const diagram = json.definitions.BPMNDiagram;
+  verifyProperties(diagram.BPMNPlane, ['bpmnElement', 'BPMNShape', 'BPMNEdge']);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyDiagram(json: any): any {
+  const diagram = json.definitions.BPMNDiagram;
+  verifyProperties(diagram, ['id', 'name', 'BPMNPlane', 'BPMNLabelStyle']);
+  return diagram;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyEdges(json: any, expected: number, edgeIndexToVerify: number): void {
+  const diagram = json.definitions.BPMNDiagram;
+  verifyIsNotEmptyArray(diagram.BPMNPlane.BPMNEdge, expected);
+
+  const edge = diagram.BPMNPlane.BPMNEdge[edgeIndexToVerify];
+  verifyProperties(edge, ['id', 'bpmnElement', 'waypoint', 'BPMNLabel']);
+  verifyIsNotEmptyArray(edge.waypoint, 3);
+  verifyProperties(edge.BPMNLabel, ['id', 'labelStyle', 'Bounds']);
+  verifyProperties(edge.BPMNLabel.Bounds, ['height', 'width', 'x', 'y']);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyStyle(json: any, expected: number) {
+  const diagram = json.definitions.BPMNDiagram;
+  verifyIsNotEmptyArray(diagram.BPMNLabelStyle, expected);
+  const style = diagram.BPMNLabelStyle[0];
+  verifyProperties(style, ['Font']);
+  expect(style.Font.name).toEqual('Segoe UI');
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyIoSpecification(json: any, expectedInputSetId: string, expectedOutputSetId: string) {
+  const process = json.definitions.process;
+  verifyProperties(process.ioSpecification, ['id', 'inputSet', 'outputSet']);
+  expect(process.ioSpecification.inputSet.id).toEqual(expectedInputSetId);
+  expect(process.ioSpecification.outputSet.id).toEqual(expectedOutputSetId);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyStartEvent(json: any, expectedOutgoing: string) {
+  const startEvent = json.definitions.process.startEvent;
+  verifyProperties(startEvent, ['id', 'name', 'isInterrupting', 'extensionElements', 'outgoing'], ['incoming']);
+  expect(startEvent.outgoing).toEqual(expectedOutgoing);
+  verifyProperties(startEvent.extensionElements, ['graphStyle']);
+  verifyProperties(startEvent.extensionElements.graphStyle, ['basic']);
+  verifyProperties(startEvent.extensionElements.graphStyle.basic, ['background', 'foreground', 'autoResize', 'borderColor', 'collapsed']);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifySequenceFlow(json: any, expected: number) {
+  const process = json.definitions.process;
+  verifyIsNotEmptyArray(process.sequenceFlow, expected);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyExclusiveGateway(json: any, expected: number) {
+  const process = json.definitions.process;
+  verifyIsNotEmptyArray(process.exclusiveGateway, expected);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyEndEvent(json: any, expectedIncomings: string[]) {
+  const endEvent = json.definitions.process.endEvent;
+  verifyProperties(endEvent, ['id', 'name', 'extensionElements', 'incoming'], ['outgoing']);
+
+  expectedIncomings.forEach((expectedIncoming, index) => expect(endEvent.incoming[index]).toEqual(expectedIncoming));
+
+  verifyProperties(endEvent.extensionElements, ['graphStyle']);
+  verifyProperties(endEvent.extensionElements.graphStyle, ['basic']);
+  verifyProperties(endEvent.extensionElements.graphStyle.basic, ['background', 'foreground', 'autoResize', 'borderColor', 'collapsed']);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyTask(json: any, expected: number) {
+  const process = json.definitions.process;
+  verifyIsNotEmptyArray(process.task, expected);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function verifyExtensionElements(json: any) {
+  const process = json.definitions.process;
+
+  verifyProperties(process.extensionElements, ['graphStyle']);
+  verifyProperties(process.extensionElements.graphStyle, ['basic', 'root']);
+  verifyProperties(process.extensionElements.graphStyle.root, [
+    'gridVisible',
+    'snapToGrid',
+    'rulerVisible',
+    'snapToGuide',
+    'rulerUnit',
+    'Grid',
+    'VerticalRuler',
+    'HorizontalRuler',
+  ]);
+  verifyProperties(process.extensionElements.graphStyle.root.Grid, ['spacing', 'color']);
 }
