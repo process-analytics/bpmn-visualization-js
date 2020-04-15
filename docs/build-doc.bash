0508a8bfcc11441ca87985e1cdfa693b0a898ee6
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-15 13:34:55 +0200

    [DOC] html doc generation (#175)
    - introduce a top level index page with introduction which includes all other
    pages
    - architecture: introduce an index page as well
    - move pages and images in their dedicated folders
    - provide a bash script relying on Docker to build the html documentation
    - update the README accordingly

---
+++ docs/build-doc.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Building bpmn-visualization-js html documentation with Docker"
+
+doc_input="$(pwd)/docs"
+doc_output="$(pwd)/build/docs"
+rm -rf ${doc_output}
+mkdir -p ${doc_output}
+
+docker run --rm -v "${doc_input}:/documents/" -v "${doc_output}:/documents-generated/" \
+    --name bpmn-visu-js-asciidoc \
+    asciidoctor/docker-asciidoctor:1.1.0 \
+    asciidoctor -D /documents-generated index.adoc
+
+cp -R ${doc_input}/architecture/images ${doc_output}/images
+
+echo "Documentation is now available in ${doc_output}"
