7fcc221edf4391d1fcbc46baace41b23fef4fb68
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-04-23 02:17:33 +0200

    refactoring: introduce a mxXmlCanvas2D to manage scale factor
    The adapter passes the scale values to the underlying canvas, so writing code is
    much more simpler: we pass regular values to the adapter instead of computing
    the scaled values each time prior to pass them

--- src/component/mxgraph/shape/task-shapes.ts
+++ src/component/mxgraph/shape/task-shapes.ts
@@ -18,6 +18,35 @@ import { MxGraphFactoryService } from '../../../service/MxGraphFactoryService';
 import { mxgraph } from 'ts-mxgraph';
 import { StyleConstant } from '../StyleConfigurator';
 
+// TODO rename + move out of this file for reuse in other shapes
+class MxCanvas2DScaler {
+  constructor(readonly c: mxgraph.mxXmlCanvas2D, readonly scaleFactor: number) {}
+
+  arcTo(rx: number, ry: number, angle: number, largeArcFlag: number, sweepFlag: number, x: number, y: number): void {
+    this.c.arcTo(rx * this.scaleFactor, ry * this.scaleFactor, angle, largeArcFlag, sweepFlag, x * this.scaleFactor, y * this.scaleFactor);
+  }
+
+  begin(): void {
+    this.c.begin();
+  }
+
+  close(): void {
+    this.c.close();
+  }
+
+  fillAndStroke(): void {
+    this.c.fillAndStroke();
+  }
+
+  lineTo(x: number, y: number): void {
+    this.c.lineTo(x * this.scaleFactor, y * this.scaleFactor);
+  }
+
+  moveTo(x: number, y: number): void {
+    this.c.moveTo(x * this.scaleFactor, y * this.scaleFactor);
+  }
+}
+
 const mxRectangleShape: typeof mxgraph.mxRectangleShape = MxGraphFactoryService.getMxGraphProperty('mxRectangleShape');
 
 abstract class BaseTaskShape extends mxRectangleShape {
@@ -83,96 +112,99 @@ export class ServiceTaskShape extends BaseTaskShape {
   }
 
   private drawIconBackground(c: mxgraph.mxXmlCanvas2D, scaleFactor: number): void {
-    c.begin();
-    c.moveTo(2.06 * scaleFactor, 24.62 * scaleFactor);
-    c.lineTo(10.17 * scaleFactor, 30.95 * scaleFactor);
-    c.lineTo(9.29 * scaleFactor, 37.73 * scaleFactor);
-    c.lineTo(0 * scaleFactor, 41.42 * scaleFactor);
-    c.lineTo(2.95 * scaleFactor, 54.24 * scaleFactor);
-    c.lineTo(13.41 * scaleFactor, 52.92 * scaleFactor);
-    c.lineTo(17.39 * scaleFactor, 58.52 * scaleFactor);
-    c.lineTo(13.56 * scaleFactor, 67.66 * scaleFactor);
-    c.lineTo(24.47 * scaleFactor, 74.44 * scaleFactor);
-    c.lineTo(30.81 * scaleFactor, 66.33 * scaleFactor);
-    c.lineTo(37.88 * scaleFactor, 67.21 * scaleFactor);
-    c.lineTo(41.57 * scaleFactor, 76.5 * scaleFactor);
-    c.lineTo(54.24 * scaleFactor, 73.55 * scaleFactor);
-    c.lineTo(53.06 * scaleFactor, 62.94 * scaleFactor);
-    c.lineTo(58.52 * scaleFactor, 58.52 * scaleFactor);
-    c.lineTo(67.21 * scaleFactor, 63.09 * scaleFactor);
-    c.lineTo(74.58 * scaleFactor, 51.88 * scaleFactor);
-    c.lineTo(66.03 * scaleFactor, 45.25 * scaleFactor);
-    c.lineTo(66.92 * scaleFactor, 38.62 * scaleFactor);
-    c.lineTo(76.5 * scaleFactor, 34.93 * scaleFactor);
-    c.lineTo(73.7 * scaleFactor, 22.26 * scaleFactor);
-    c.lineTo(62.64 * scaleFactor, 23.44 * scaleFactor);
-    c.lineTo(58.81 * scaleFactor, 18.42 * scaleFactor);
-    c.lineTo(62.79 * scaleFactor, 8.7 * scaleFactor);
-    c.lineTo(51.74 * scaleFactor, 2.21 * scaleFactor);
-    c.lineTo(44.81 * scaleFactor, 10.47 * scaleFactor);
-    c.lineTo(38.03 * scaleFactor, 9.43 * scaleFactor);
-    c.lineTo(33.75 * scaleFactor, 0 * scaleFactor);
-    c.lineTo(21.52 * scaleFactor, 3.24 * scaleFactor);
-    c.lineTo(22.7 * scaleFactor, 13.56 * scaleFactor);
-    c.lineTo(18.13 * scaleFactor, 17.54 * scaleFactor);
-    c.lineTo(8.7 * scaleFactor, 13.56 * scaleFactor);
-    c.close();
+    const canvas = new MxCanvas2DScaler(c, scaleFactor);
 
-    const arc1Ray = 13.5 * scaleFactor;
-    const arc1StartX = 24.8 * scaleFactor;
-    const arc1StartY = 39 * scaleFactor;
-    c.moveTo(arc1StartX, arc1StartY);
-    c.arcTo(arc1Ray, arc1Ray, 0, 1, 1, arc1StartX + 2 * arc1Ray, arc1StartY);
-    c.arcTo(arc1Ray, arc1Ray, 0, 0, 1, arc1StartX, arc1StartY);
-    c.close();
-    c.fillAndStroke();
+    canvas.begin();
+    canvas.moveTo(2.06, 24.62);
+    canvas.lineTo(10.17, 30.95);
+    canvas.lineTo(9.29, 37.73);
+    canvas.lineTo(0, 41.42);
+    canvas.lineTo(2.95, 54.24);
+    canvas.lineTo(13.41, 52.92);
+    canvas.lineTo(17.39, 58.52);
+    canvas.lineTo(13.56, 67.66);
+    canvas.lineTo(24.47, 74.44);
+    canvas.lineTo(30.81, 66.33);
+    canvas.lineTo(37.88, 67.21);
+    canvas.lineTo(41.57, 76.5);
+    canvas.lineTo(54.24, 73.55);
+    canvas.lineTo(53.06, 62.94);
+    canvas.lineTo(58.52, 58.52);
+    canvas.lineTo(67.21, 63.09);
+    canvas.lineTo(74.58, 51.88);
+    canvas.lineTo(66.03, 45.25);
+    canvas.lineTo(66.92, 38.62);
+    canvas.lineTo(76.5, 34.93);
+    canvas.lineTo(73.7, 22.26);
+    canvas.lineTo(62.64, 23.44);
+    canvas.lineTo(58.81, 18.42);
+    canvas.lineTo(62.79, 8.7);
+    canvas.lineTo(51.74, 2.21);
+    canvas.lineTo(44.81, 10.47);
+    canvas.lineTo(38.03, 9.43);
+    canvas.lineTo(33.75, 0);
+    canvas.lineTo(21.52, 3.24);
+    canvas.lineTo(22.7, 13.56);
+    canvas.lineTo(18.13, 17.54);
+    canvas.lineTo(8.7, 13.56);
+    canvas.close();
+
+    const arc1Ray = 13.5;
+    const arc1StartX = 24.8;
+    const arc1StartY = 39;
+    canvas.moveTo(arc1StartX, arc1StartY);
+    canvas.arcTo(arc1Ray, arc1Ray, 0, 1, 1, arc1StartX + 2 * arc1Ray, arc1StartY);
+    canvas.arcTo(arc1Ray, arc1Ray, 0, 0, 1, arc1StartX, arc1StartY);
+    canvas.close();
+    canvas.fillAndStroke();
   }
 
   private drawIconForeground(c: mxgraph.mxXmlCanvas2D, scaleFactor: number): void {
-    c.begin();
-    c.moveTo(16.46 * scaleFactor, 41.42 * scaleFactor);
-    c.lineTo(24.57 * scaleFactor, 47.75 * scaleFactor);
-    c.lineTo(23.69 * scaleFactor, 54.53 * scaleFactor);
-    c.lineTo(14.4 * scaleFactor, 58.22 * scaleFactor);
-    c.lineTo(17.35 * scaleFactor, 71.04 * scaleFactor);
-    c.lineTo(27.81 * scaleFactor, 69.72 * scaleFactor);
-    c.lineTo(31.79 * scaleFactor, 75.32 * scaleFactor);
-    c.lineTo(27.96 * scaleFactor, 84.46 * scaleFactor);
-    c.lineTo(38.87 * scaleFactor, 91.24 * scaleFactor);
-    c.lineTo(45.21 * scaleFactor, 83.13 * scaleFactor);
-    c.lineTo(52.28 * scaleFactor, 84.01 * scaleFactor);
-    c.lineTo(55.97 * scaleFactor, 93.3 * scaleFactor);
-    c.lineTo(68.64 * scaleFactor, 90.35 * scaleFactor);
-    c.lineTo(67.46 * scaleFactor, 79.74 * scaleFactor);
-    c.lineTo(72.92 * scaleFactor, 75.32 * scaleFactor);
-    c.lineTo(81.61 * scaleFactor, 79.89 * scaleFactor);
-    c.lineTo(88.98 * scaleFactor, 68.68 * scaleFactor);
-    c.lineTo(80.43 * scaleFactor, 62.05 * scaleFactor);
-    c.lineTo(81.32 * scaleFactor, 55.42 * scaleFactor);
-    c.lineTo(90.9 * scaleFactor, 51.73 * scaleFactor);
-    c.lineTo(88.1 * scaleFactor, 39.06 * scaleFactor);
-    c.lineTo(77.04 * scaleFactor, 40.24 * scaleFactor);
-    c.lineTo(73.21 * scaleFactor, 35.22 * scaleFactor);
-    c.lineTo(77.19 * scaleFactor, 25.5 * scaleFactor);
-    c.lineTo(66.14 * scaleFactor, 19.01 * scaleFactor);
-    c.lineTo(59.21 * scaleFactor, 27.27 * scaleFactor);
-    c.lineTo(52.43 * scaleFactor, 26.23 * scaleFactor);
-    c.lineTo(48.15 * scaleFactor, 16.8 * scaleFactor);
-    c.lineTo(35.92 * scaleFactor, 20.04 * scaleFactor);
-    c.lineTo(37.1 * scaleFactor, 30.36 * scaleFactor);
-    c.lineTo(32.53 * scaleFactor, 34.34 * scaleFactor);
-    c.lineTo(23.1 * scaleFactor, 30.36 * scaleFactor);
-    c.close();
+    const canvas = new MxCanvas2DScaler(c, scaleFactor);
 
-    const arc2Ray = 13.5 * scaleFactor;
-    const arc2StartX = 39.2 * scaleFactor;
-    const arc2StartY = 55.8 * scaleFactor;
-    c.moveTo(arc2StartX, arc2StartY);
-    c.arcTo(arc2Ray, arc2Ray, 0, 1, 1, arc2StartX + 2 * arc2Ray, arc2StartY);
-    c.arcTo(arc2Ray, arc2Ray, 0, 0, 1, arc2StartX, arc2StartY);
+    canvas.begin();
+    canvas.moveTo(16.46, 41.42);
+    canvas.lineTo(24.57, 47.75);
+    canvas.lineTo(23.69, 54.53);
+    canvas.lineTo(14.4, 58.22);
+    canvas.lineTo(17.35, 71.04);
+    canvas.lineTo(27.81, 69.72);
+    canvas.lineTo(31.79, 75.32);
+    canvas.lineTo(27.96, 84.46);
+    canvas.lineTo(38.87, 91.24);
+    canvas.lineTo(45.21, 83.13);
+    canvas.lineTo(52.28, 84.01);
+    canvas.lineTo(55.97, 93.3);
+    canvas.lineTo(68.64, 90.35);
+    canvas.lineTo(67.46, 79.74);
+    canvas.lineTo(72.92, 75.32);
+    canvas.lineTo(81.61, 79.89);
+    canvas.lineTo(88.98, 68.68);
+    canvas.lineTo(80.43, 62.05);
+    canvas.lineTo(81.32, 55.42);
+    canvas.lineTo(90.9, 51.73);
+    canvas.lineTo(88.1, 39.06);
+    canvas.lineTo(77.04, 40.24);
+    canvas.lineTo(73.21, 35.22);
+    canvas.lineTo(77.19, 25.5);
+    canvas.lineTo(66.14, 19.01);
+    canvas.lineTo(59.21, 27.27);
+    canvas.lineTo(52.43, 26.23);
+    canvas.lineTo(48.15, 16.8);
+    canvas.lineTo(35.92, 20.04);
+    canvas.lineTo(37.1, 30.36);
+    canvas.lineTo(32.53, 34.34);
+    canvas.lineTo(23.1, 30.36);
+    canvas.close();
 
-    c.close();
-    c.fillAndStroke();
+    const arc2Ray = 13.5;
+    const arc2StartX = 39.2;
+    const arc2StartY = 55.8;
+    canvas.moveTo(arc2StartX, arc2StartY);
+    canvas.arcTo(arc2Ray, arc2Ray, 0, 1, 1, arc2StartX + 2 * arc2Ray, arc2StartY);
+    canvas.arcTo(arc2Ray, arc2Ray, 0, 0, 1, arc2StartX, arc2StartY);
+    canvas.close();
+    canvas.fillAndStroke();
 
     // TODO fill the inner circle to mask the background
   }
