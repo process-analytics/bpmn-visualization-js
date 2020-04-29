cd48b3321bc1476cb0473f82239dd4718195ef5e
Céline Souchet <celine.souchet@bonitasoft.com>
2020-04-29 17:00:18 +0200

    Change the link to the draw.io shape used as starting point for our icon

--- src/component/mxgraph/shape/event-shapes.ts
+++ src/component/mxgraph/shape/event-shapes.ts
@@ -100,7 +100,7 @@ export class ThrowIntermediateEventShape extends EventShape {
   }
 
   // this implementation is adapted from the draw.io BPMN 'throwing' outlines
-  // https://github.com/jgraph/drawio/blob/master/src/main/webapp/shapes/bpmn/mxBpmnShape2.js#L431
+  // https://github.com/jgraph/drawio/blob/0e19be6b42755790a749af30450c78c0d83be765/src/main/webapp/shapes/bpmn/mxBpmnShape2.js#L431
   protected paintOuterShape(c: mxgraph.mxXmlCanvas2D, x: number, y: number, w: number, h: number): void {
     c.ellipse(x, y, w, h);
     c.fillAndStroke();
