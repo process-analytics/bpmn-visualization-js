c697f3dcfbc8e864196715fc29de61c0e7849e7e
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-06-04 17:08:39 +0200

    Do not use mxgraphFactory from 'ts-mxgraph'; - mxGraph loaded externally (not embedded) !e2e tests to be fixed

--- test/e2e/mxGraph.model.test.ts
+++ test/e2e/mxGraph.model.test.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import BpmnVisu from '../../src/component/BpmnVisu';
 import { ShapeBpmnElementKind } from '../../src/model/bpmn/shape/ShapeBpmnElementKind';
 import { mxgraph } from 'ts-mxgraph';
