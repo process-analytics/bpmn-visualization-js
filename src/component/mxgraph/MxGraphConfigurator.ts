1809c4c09f845f0601d188f40c558eb4ee4b1e47
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-06-09 12:48:53 +0200

    tmp: relax cells locked/selectable to see edge lablel anchors

--- src/component/mxgraph/MxGraphConfigurator.ts
+++ src/component/mxgraph/MxGraphConfigurator.ts
@@ -45,12 +45,12 @@ export default class MxGraphConfigurator {
   }
 
   private configureGraph(): void {
-    this.graph.setCellsLocked(true);
-    this.graph.setCellsSelectable(false);
+    this.graph.setCellsLocked(false);
+    this.graph.setCellsSelectable(true);
     this.graph.htmlLabels = true; // required for wrapping and clipping
 
     // TODO see if this changes something
-    this.graph.setEdgeLabelsMovable(false);
+    this.graph.setEdgeLabelsMovable(true);
     this.graph.setVertexLabelsMovable(false);
   }
 }
