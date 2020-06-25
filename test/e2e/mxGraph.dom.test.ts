1a2d7c9eec601b30d5c708ca6eae2d0ae2cca6e8
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-06-25 12:50:36 +0200

    dom only tests: load bpmn content from external file

--- test/e2e/mxGraph.dom.test.ts
+++ test/e2e/mxGraph.dom.test.ts
@@ -13,76 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as fs from 'fs';
+import * as path from 'path';
 import BpmnVisu from '../../src/component/BpmnVisu';
+
+const bpmnStartTaskEnd = fs.readFileSync(path.join(__dirname, '../fixtures/bpmn/simple-start-task-end.bpmn'), 'utf-8');
+const graphContainerId = 'bpmn-visualization-graph';
 
 function initializeBpmnVisu(): BpmnVisu {
   // insert graph container
   const containerDiv = document.createElement('div');
-  containerDiv.id = 'graph-test';
+  containerDiv.id = graphContainerId;
   document.body.insertBefore(containerDiv, document.body.firstChild);
   // initialize graph
-  const graphTest = document.getElementById('graph-test');
-  return new BpmnVisu(graphTest);
-}
-
-// TODO duplication with view tests
-function bpmnStartTaskEnd(): string {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<bpmn:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" id="Definitions_0x0opj6" targetNamespace="http://example.bpmn.com/schema/bpmn">
-  <bpmn:process id="Process_1" isExecutable="false">
-    <bpmn:startEvent id="StartEvent_1" name="Start Event 1">
-      <bpmn:outgoing>Flow_1</bpmn:outgoing>
-    </bpmn:startEvent>
-    <bpmn:sequenceFlow id="Flow_1" sourceRef="StartEvent_1" targetRef="Activity_1" />
-    <bpmn:task id="Activity_1" name="Task 1">
-      <bpmn:incoming>Flow_1</bpmn:incoming>
-      <bpmn:outgoing>Flow_2</bpmn:outgoing>
-    </bpmn:task>
-    <bpmn:endEvent id="EndEvent_1" name="End Event 1">
-      <bpmn:incoming>Flow_2</bpmn:incoming>
-    </bpmn:endEvent>
-    <bpmn:sequenceFlow id="Flow_2" sourceRef="Activity_1" targetRef="EndEvent_1" />
-  </bpmn:process>
-  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
-      <bpmndi:BPMNEdge id="BPMNEdge_Flow_1" bpmnElement="Flow_1">
-        <di:waypoint x="192" y="99" />
-        <di:waypoint x="250" y="99" />
-      </bpmndi:BPMNEdge>
-      <bpmndi:BPMNEdge id="BPMNEdge_Flow_2" bpmnElement="Flow_2">
-        <di:waypoint x="350" y="99" />
-        <di:waypoint x="412" y="99" />
-      </bpmndi:BPMNEdge>
-      <bpmndi:BPMNShape id="BPMNShape_StartEvent_1" bpmnElement="StartEvent_1">
-        <dc:Bounds x="156" y="81" width="36" height="36" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="158" y="124" width="33" height="14" />
-        </bpmndi:BPMNLabel>
-      </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id="BPMNShape_Activity_1" bpmnElement="Activity_1">
-        <dc:Bounds x="250" y="59" width="100" height="80" />
-      </bpmndi:BPMNShape>
-      <bpmndi:BPMNShape id="BPMNShape_EndEvent_1" bpmnElement="EndEvent_1">
-        <dc:Bounds x="412" y="81" width="36" height="36" />
-        <bpmndi:BPMNLabel>
-          <dc:Bounds x="416" y="124" width="29" height="14" />
-        </bpmndi:BPMNLabel>
-      </bpmndi:BPMNShape>
-    </bpmndi:BPMNPlane>
-  </bpmndi:BPMNDiagram>
-</bpmn:definitions>
-`;
+  const bpmnVisuGraphContainer = document.getElementById(graphContainerId);
+  return new BpmnVisu(bpmnVisuGraphContainer);
 }
 
 function expectEvent(cellId: string): void {
-  const cellGroups = document.querySelectorAll(`#graph-test svg g g[data-cell-id="${cellId}"]`);
+  const cellGroups = document.querySelectorAll(`#${graphContainerId} svg g g[data-cell-id="${cellId}"]`);
   const event = cellGroups[0] as SVGGElement;
   const shape = event.firstChild as SVGEllipseElement;
   expect(shape.nodeName).toBe('ellipse');
 }
 
 function expectTask(cellId: string): void {
-  const cellGroups = document.querySelectorAll(`#graph-test svg g g[data-cell-id="${cellId}"]`);
+  const cellGroups = document.querySelectorAll(`#${graphContainerId} svg g g[data-cell-id="${cellId}"]`);
   const task = cellGroups[0] as SVGGElement;
   const shape = task.firstChild as SVGGElement;
   expect(shape.nodeName).toBe('rect');
@@ -92,7 +48,7 @@ describe('BpmnVisu dom only checks', () => {
   const bpmnVisu = initializeBpmnVisu();
 
   it('DOM should contains BPMN elements', async () => {
-    bpmnVisu.load(bpmnStartTaskEnd());
+    bpmnVisu.load(bpmnStartTaskEnd);
 
     expectEvent('StartEvent_1');
     expectTask('Activity_1');
