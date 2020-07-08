512903834ba023e5892211a4f452b26e96c20667
Céline Souchet <celine.souchet@bonitasoft.com>
2020-07-08 18:27:01 +0200

    Refactor unit tests for all gateways (same test file)

--- test/unit/component/parser/json/BpmnJsonParser.gateway.test.ts
+++ test/unit/component/parser/json/BpmnJsonParser.gateway.test.ts
@@ -16,22 +16,26 @@
 import { ShapeBpmnElementKind } from '../../../../../src/model/bpmn/shape/ShapeBpmnElementKind';
 import { parseJsonAndExpectOnlyFlowNodes, verifyShape } from './JsonTestUtils';
 
-describe('parse bpmn as json for inclusive gateway', () => {
-  it('json containing one process with a single inclusive gateway', () => {
+describe.each([
+  ['exclusive', ShapeBpmnElementKind.GATEWAY_EXCLUSIVE],
+  ['inclusive', ShapeBpmnElementKind.GATEWAY_INCLUSIVE],
+  ['parallel', ShapeBpmnElementKind.GATEWAY_PARALLEL],
+])('parse bpmn as json for %s gateways', (bpmnKind: string, expectedShapeBpmnElementKind: ShapeBpmnElementKind) => {
+  it(`json containing one process with a single ${bpmnKind} gateway`, () => {
     const json = `{
                 "definitions" : {
                     "process": {
-                        "inclusiveGateway": {
-                            "id":"inclusiveGateway_id_0",
-                            "name":"inclusiveGateway name"
+                        "${bpmnKind}Gateway": {
+                            "id":"${bpmnKind}Gateway_id_0",
+                            "name":"${bpmnKind}Gateway name"
                         }
                     },
                     "BPMNDiagram": {
                         "name":"process 0",
                         "BPMNPlane": {
                             "BPMNShape": {
-                                "id":"shape_inclusiveGateway_id_0",
-                                "bpmnElement":"inclusiveGateway_id_0",
+                                "id":"shape_${bpmnKind}Gateway_id_0",
+                                "bpmnElement":"${bpmnKind}Gateway_id_0",
                                 "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
                             }
                         }
@@ -42,10 +46,10 @@ describe('parse bpmn as json for inclusive gateway', () => {
     const model = parseJsonAndExpectOnlyFlowNodes(json, 1);
 
     verifyShape(model.flowNodes[0], {
-      shapeId: 'shape_inclusiveGateway_id_0',
-      bpmnElementId: 'inclusiveGateway_id_0',
-      bpmnElementName: 'inclusiveGateway name',
-      bpmnElementKind: ShapeBpmnElementKind.GATEWAY_INCLUSIVE,
+      shapeId: `shape_${bpmnKind}Gateway_id_0`,
+      bpmnElementId: `${bpmnKind}Gateway_id_0`,
+      bpmnElementName: `${bpmnKind}Gateway name`,
+      bpmnElementKind: expectedShapeBpmnElementKind,
       bounds: {
         x: 362,
         y: 232,
@@ -55,14 +59,14 @@ describe('parse bpmn as json for inclusive gateway', () => {
     });
   });
 
-  it('json containing one process declared as array with a single inclusive gateway', () => {
+  it(`json containing one process declared as array with a single ${bpmnKind} gateway`, () => {
     const json = `{
                 "definitions": {
                     "process": [
                         {
-                            "inclusiveGateway": {
-                                "id":"inclusiveGateway_id_1",
-                                "name":"inclusiveGateway name"
+                            "${bpmnKind}Gateway": {
+                                "id":"${bpmnKind}Gateway_id_1",
+                                "name":"${bpmnKind}Gateway name"
                             }
                         }
                     ],
@@ -70,8 +74,8 @@ describe('parse bpmn as json for inclusive gateway', () => {
                         "name":"process 0",
                         "BPMNPlane": {
                             "BPMNShape": {
-                                "id":"shape_inclusiveGateway_id_1",
-                                "bpmnElement":"inclusiveGateway_id_1",
+                                "id":"shape_${bpmnKind}Gateway_id_1",
+                                "bpmnElement":"${bpmnKind}Gateway_id_1",
                                 "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
                             }
                         }
@@ -82,10 +86,10 @@ describe('parse bpmn as json for inclusive gateway', () => {
     const model = parseJsonAndExpectOnlyFlowNodes(json, 1);
 
     verifyShape(model.flowNodes[0], {
-      shapeId: 'shape_inclusiveGateway_id_1',
-      bpmnElementId: 'inclusiveGateway_id_1',
-      bpmnElementName: 'inclusiveGateway name',
-      bpmnElementKind: ShapeBpmnElementKind.GATEWAY_INCLUSIVE,
+      shapeId: `shape_${bpmnKind}Gateway_id_1`,
+      bpmnElementId: `${bpmnKind}Gateway_id_1`,
+      bpmnElementName: `${bpmnKind}Gateway name`,
+      bpmnElementKind: expectedShapeBpmnElementKind,
       bounds: {
         x: 362,
         y: 232,
@@ -95,16 +99,16 @@ describe('parse bpmn as json for inclusive gateway', () => {
     });
   });
 
-  it('json containing one process with an array of inclusive gateways with name & without name', () => {
+  it(`json containing one process with an array of ${bpmnKind} gateways with name & without name`, () => {
     const json = `{
                 "definitions" : {
                     "process": {
-                        "inclusiveGateway": [
+                        "${bpmnKind}Gateway": [
                           {
-                              "id":"inclusiveGateway_id_0",
-                              "name":"inclusiveGateway name"
+                              "id":"${bpmnKind}Gateway_id_0",
+                              "name":"${bpmnKind}Gateway name"
                           }, {
-                              "id":"inclusiveGateway_id_1"
+                              "id":"${bpmnKind}Gateway_id_1"
                           }
                         ]
                     },
@@ -113,12 +117,12 @@ describe('parse bpmn as json for inclusive gateway', () => {
                         "BPMNPlane": {
                             "BPMNShape": [
                               {
-                                "id":"shape_inclusiveGateway_id_0",
-                                "bpmnElement":"inclusiveGateway_id_0",
+                                "id":"shape_${bpmnKind}Gateway_id_0",
+                                "bpmnElement":"${bpmnKind}Gateway_id_0",
                                 "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
                               }, {
-                                "id":"shape_inclusiveGateway_id_1",
-                                "bpmnElement":"inclusiveGateway_id_1",
+                                "id":"shape_${bpmnKind}Gateway_id_1",
+                                "bpmnElement":"${bpmnKind}Gateway_id_1",
                                 "Bounds": { "x": 365, "y": 235, "width": 35, "height": 46 }
                               }
                             ]
@@ -130,10 +134,10 @@ describe('parse bpmn as json for inclusive gateway', () => {
     const model = parseJsonAndExpectOnlyFlowNodes(json, 2);
 
     verifyShape(model.flowNodes[0], {
-      shapeId: 'shape_inclusiveGateway_id_0',
-      bpmnElementId: 'inclusiveGateway_id_0',
-      bpmnElementName: 'inclusiveGateway name',
-      bpmnElementKind: ShapeBpmnElementKind.GATEWAY_INCLUSIVE,
+      shapeId: `shape_${bpmnKind}Gateway_id_0`,
+      bpmnElementId: `${bpmnKind}Gateway_id_0`,
+      bpmnElementName: `${bpmnKind}Gateway name`,
+      bpmnElementKind: expectedShapeBpmnElementKind,
       bounds: {
         x: 362,
         y: 232,
@@ -142,10 +146,10 @@ describe('parse bpmn as json for inclusive gateway', () => {
       },
     });
     verifyShape(model.flowNodes[1], {
-      shapeId: 'shape_inclusiveGateway_id_1',
-      bpmnElementId: 'inclusiveGateway_id_1',
+      shapeId: `shape_${bpmnKind}Gateway_id_1`,
+      bpmnElementId: `${bpmnKind}Gateway_id_1`,
       bpmnElementName: undefined,
-      bpmnElementKind: ShapeBpmnElementKind.GATEWAY_INCLUSIVE,
+      bpmnElementKind: expectedShapeBpmnElementKind,
       bounds: {
         x: 365,
         y: 235,
