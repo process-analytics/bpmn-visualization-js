c071a62c5bce3023e9a39553c3bdd757482da413
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-07-15 15:22:11 +0200

    [REFACTOR] single `painted icon size` computation function
    This makes usage of the BpmnCanvasConfiguration easier: no more parameter to
    configure the way the computation is done, everything is managed internally by
    BpmnCanvas.
    Tests have been added for the computation function.

---
+++ test/unit/component/mxgraph/render/BpmnCanvas.test.ts
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2020 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { IconStyleConfiguration, ShapeConfiguration, Size } from '../../../../../src/component/mxgraph/shape/render/render-types';
+import { computeScaledIconSize } from '../../../../../src/component/mxgraph/shape/render/BpmnCanvas';
+
+describe('compute scaled icon size', () => {
+  function newIconStyleConfiguration(strokeWidth = 0): IconStyleConfiguration {
+    return {
+      fillColor: 'do_not_care',
+      isFilled: false,
+      strokeColor: 'do_not_care',
+      strokeWidth: strokeWidth,
+      margin: 0,
+    };
+  }
+  function newShapeConfiguration(w: number, h: number): ShapeConfiguration {
+    return {
+      x: -1, // not relevant here
+      y: -1, // not relevant here
+      w: w,
+      h: h,
+    };
+  }
+  // TODO add expect size helper function
+  function expectSize(actual: Size, expected: Size): void {
+    expect(actual.width).toEqual(expected.width);
+    expect(actual.height).toEqual(expected.height);
+  }
+
+  describe('parent square dimension', () => {
+    test('original square icon', () => {
+      const size = computeScaledIconSize({ width: 100, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(50, 50), 0.25);
+      expectSize(size, { width: 12.5, height: 12.5 }); // max 12.5 12.5
+    });
+    test('original icon - width larger than height', () => {
+      const size = computeScaledIconSize({ width: 200, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(50, 50), 0.25);
+      expectSize(size, { width: 12.5, height: 6.25 }); // max 12.5 12.5
+    });
+    test('original icon - height larger than width', () => {
+      const size = computeScaledIconSize({ width: 100, height: 200 }, newIconStyleConfiguration(), newShapeConfiguration(50, 50), 0.25);
+      expectSize(size, { width: 6.25, height: 12.5 }); // max 12.5 12.5
+    });
+  });
+
+  describe('parent width larger than height', () => {
+    test('original square icon', () => {
+      const size = computeScaledIconSize({ width: 100, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(100, 80), 0.25);
+      expectSize(size, { width: 20, height: 20 }); // max 25 20
+    });
+    test('original icon - width larger than height', () => {
+      const size = computeScaledIconSize({ width: 200, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(100, 80), 0.25);
+      expectSize(size, { width: 25, height: 12.5 }); // max 25 20
+    });
+    test('original icon - height larger than width', () => {
+      const size = computeScaledIconSize({ width: 100, height: 200 }, newIconStyleConfiguration(), newShapeConfiguration(100, 80), 0.25);
+      expectSize(size, { width: 10, height: 20 }); // max 25 20
+    });
+  });
+
+  describe('parent height larger than width', () => {
+    test('original square icon', () => {
+      const size = computeScaledIconSize({ width: 100, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(80, 100), 0.25);
+      expectSize(size, { width: 20, height: 20 }); // max 25 20
+    });
+    test('original icon - width larger than height', () => {
+      const size = computeScaledIconSize({ width: 200, height: 100 }, newIconStyleConfiguration(), newShapeConfiguration(80, 100), 0.25);
+      expectSize(size, { width: 20, height: 10 }); // max 25 20
+    });
+    test('original icon - height larger than width', () => {
+      const size = computeScaledIconSize({ width: 100, height: 200 }, newIconStyleConfiguration(), newShapeConfiguration(80, 100), 0.25);
+      expectSize(size, { width: 12.5, height: 25 }); // max 25 20
+    });
+  });
+});
