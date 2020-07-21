d47026c2bf105b18966458494204ab578b9515a6
Céline Souchet <celine.souchet@bonitasoft.com>
2020-07-21 11:14:38 +0200

    Replace string content for BPMN content by json object, in unit json tests: Gateways

--- test/unit/component/parser/json/BpmnJsonParser.gateway.test.ts
+++ test/unit/component/parser/json/BpmnJsonParser.gateway.test.ts
@@ -14,41 +14,51 @@
  * limitations under the License.
  */
 import { ShapeBpmnElementKind } from '../../../../../src/model/bpmn/shape/ShapeBpmnElementKind';
-import { parseJsonAndExpectOnlyFlowNodes, verifyShape } from './JsonTestUtils';
+// import { parseJsonAndExpectOnlyFlowNodes, verifyShape } from './JsonTestUtils';
+import { verifyShape } from './JsonTestUtils';
+import { TProcess } from '../../../../../src/component/parser/xml/bpmn-json-model/baseElement/rootElement/rootElement';
+import { defaultBpmnJsonParser } from '../../../../../src/component/parser/json/BpmnJsonParser';
+import { BpmnJsonModel } from '../../../../../src/component/parser/xml/bpmn-json-model/BPMN20';
 
 describe.each([
   ['exclusive', ShapeBpmnElementKind.GATEWAY_EXCLUSIVE],
   ['inclusive', ShapeBpmnElementKind.GATEWAY_INCLUSIVE],
   ['parallel', ShapeBpmnElementKind.GATEWAY_PARALLEL],
 ])('parse bpmn as json for %s gateway', (bpmnKind: string, expectedShapeBpmnElementKind: ShapeBpmnElementKind) => {
-  const processJsonAsObjectWithGatewayJsonAsObject = `{
-                        "${bpmnKind}Gateway": {
-                            "id":"${bpmnKind}Gateway_id_0",
-                            "name":"${bpmnKind}Gateway name"
-                        }
-                    }`;
+  const processJsonAsObjectWithGatewayJsonAsObject = {} as TProcess;
+  processJsonAsObjectWithGatewayJsonAsObject[`${bpmnKind}Gateway`] = {
+    id: `${bpmnKind}Gateway_id_0`,
+    name: `${bpmnKind}Gateway name`,
+  };
 
   it.each([
-    ['object', `${processJsonAsObjectWithGatewayJsonAsObject}`],
-    ['array', `[${processJsonAsObjectWithGatewayJsonAsObject}]`],
-  ])(`should convert as Shape, when a ${bpmnKind} gateway is an attribute (as object) of 'process' (as %s)`, (title: string, processJson: string) => {
-    const json = `{
-                "definitions" : {
-                    "process": ${processJson},
-                    "BPMNDiagram": {
-                        "name":"process 0",
-                        "BPMNPlane": {
-                            "BPMNShape": {
-                                "id":"shape_${bpmnKind}Gateway_id_0",
-                                "bpmnElement":"${bpmnKind}Gateway_id_0",
-                                "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
-                            }
-                        }
-                    }
-                }
-            }`;
+    ['object', processJsonAsObjectWithGatewayJsonAsObject],
+    ['array', [processJsonAsObjectWithGatewayJsonAsObject]],
+  ])(`should convert as Shape, when a ${bpmnKind} gateway is an attribute (as object) of 'process' (as %s)`, (title: string, processJson: TProcess | TProcess[]) => {
+    const json = {
+      definitions: {
+        targetNamespace: '',
+        process: processJson,
+        BPMNDiagram: {
+          name: 'process 0',
+          BPMNPlane: {
+            BPMNShape: {
+              id: `shape_${bpmnKind}Gateway_id_0`,
+              bpmnElement: `${bpmnKind}Gateway_id_0`,
+              Bounds: { x: 362, y: 232, width: 36, height: 45 },
+            },
+          },
+        },
+      },
+    };
 
-    const model = parseJsonAndExpectOnlyFlowNodes(json, 1);
+    //const model = parseJsonAndExpectOnlyFlowNodes(json, 1);
+
+    const model = defaultBpmnJsonParser().parse(json);
+    expect(model.lanes).toHaveLength(0);
+    expect(model.pools).toHaveLength(0);
+    expect(model.flowNodes).toHaveLength(1);
+    expect(model.edges).toHaveLength(0);
 
     verifyShape(model.flowNodes[0], {
       shapeId: `shape_${bpmnKind}Gateway_id_0`,
@@ -65,38 +75,46 @@ describe.each([
   });
 
   it(`should convert as Shape, when a ${bpmnKind} gateway (with/without name) is an attribute (as array) of 'process'`, () => {
-    const json = `{
-                "definitions" : {
-                    "process": {
-                        "${bpmnKind}Gateway": [
-                          {
-                              "id":"${bpmnKind}Gateway_id_0",
-                              "name":"${bpmnKind}Gateway name"
-                          }, {
-                              "id":"${bpmnKind}Gateway_id_1"
-                          }
-                        ]
-                    },
-                    "BPMNDiagram": {
-                        "name":"process 0",
-                        "BPMNPlane": {
-                            "BPMNShape": [
-                              {
-                                "id":"shape_${bpmnKind}Gateway_id_0",
-                                "bpmnElement":"${bpmnKind}Gateway_id_0",
-                                "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
-                              }, {
-                                "id":"shape_${bpmnKind}Gateway_id_1",
-                                "bpmnElement":"${bpmnKind}Gateway_id_1",
-                                "Bounds": { "x": 365, "y": 235, "width": 35, "height": 46 }
-                              }
-                            ]
-                        }
-                    }
-                }
-            }`;
+    const json: BpmnJsonModel = {
+      definitions: {
+        targetNamespace: '',
+        process: {},
+        BPMNDiagram: {
+          name: 'process 0',
+          BPMNPlane: {
+            BPMNShape: [
+              {
+                id: `shape_${bpmnKind}Gateway_id_0`,
+                bpmnElement: `${bpmnKind}Gateway_id_0`,
+                Bounds: { x: 362, y: 232, width: 36, height: 45 },
+              },
+              {
+                id: `shape_${bpmnKind}Gateway_id_1`,
+                bpmnElement: `${bpmnKind}Gateway_id_1`,
+                Bounds: { x: 365, y: 235, width: 35, height: 46 },
+              },
+            ],
+          },
+        },
+      },
+    };
+    (json.definitions.process as TProcess)[`${bpmnKind}Gateway`] = [
+      {
+        id: `${bpmnKind}Gateway_id_0`,
+        name: `${bpmnKind}Gateway name`,
+      },
+      {
+        id: `${bpmnKind}Gateway_id_1`,
+      },
+    ];
 
-    const model = parseJsonAndExpectOnlyFlowNodes(json, 2);
+    //const model = parseJsonAndExpectOnlyFlowNodes(json, 2);
+
+    const model = defaultBpmnJsonParser().parse(json);
+    expect(model.lanes).toHaveLength(0);
+    expect(model.pools).toHaveLength(0);
+    expect(model.flowNodes).toHaveLength(2);
+    expect(model.edges).toHaveLength(0);
 
     verifyShape(model.flowNodes[0], {
       shapeId: `shape_${bpmnKind}Gateway_id_0`,
