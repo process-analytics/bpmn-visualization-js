1e8cd2f91f262dd2d418fb1b8fbd8459e4a339c9
Céline Souchet <celine.souchet@bonitasoft.com>
2020-07-22 10:09:28 +0200

    Replace string content for BPMN content by json object, in unit json tests: Default Sequence Flow

--- test/unit/component/parser/json/BpmnJsonParser.sequenceFlow.default.test.ts
+++ test/unit/component/parser/json/BpmnJsonParser.sequenceFlow.default.test.ts
@@ -13,52 +13,68 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { parseJsonAndExpectOnlyEdgesAndFlowNodes, verifyEdge } from './JsonTestUtils';
+import { verifyEdge } from './JsonTestUtils';
+// import { parseJsonAndExpectOnlyEdgesAndFlowNodes, verifyEdge } from './JsonTestUtils';
 import { SequenceFlowKind } from '../../../../../src/model/bpmn/edge/SequenceFlowKind';
 import each from 'jest-each';
+import { defaultBpmnJsonParser } from '../../../../../src/component/parser/json/BpmnJsonParser';
+import Waypoint from '../../../../../src/model/bpmn/edge/Waypoint';
+import { TProcess } from '../../../../../src/component/parser/xml/bpmn-json-model/baseElement/rootElement/rootElement';
 
 describe('parse bpmn as json for default sequence flow', () => {
   it('json containing one process with a default sequence flow & a sequence flow', () => {
-    const json = `{
-          "definitions": {
-              "process": {
-                  "id": "Process_1",
-                  "task": { "id": "task_id_0", "default": "sequenceFlow_id_0"},
-                  "sequenceFlow": [{
-                        "id": "sequenceFlow_id_0",
-                        "name": "label 1",
-                        "sourceRef": "task_id_0",
-                        "targetRef": "targetRef_RLk"
-                    }, {
-                        "id": "sequenceFlow_id_1",
-                        "sourceRef": "task_id_0",
-                        "targetRef": "targetRef_2"
-                    }
-                  ]
+    const json = {
+      definitions: {
+        targetNamespace: '',
+        process: {
+          id: 'Process_1',
+          task: { id: 'task_id_0', default: 'sequenceFlow_id_0' },
+          sequenceFlow: [
+            {
+              id: 'sequenceFlow_id_0',
+              name: 'label 1',
+              sourceRef: 'task_id_0',
+              targetRef: 'targetRef_RLk',
+            },
+            {
+              id: 'sequenceFlow_id_1',
+              sourceRef: 'task_id_0',
+              targetRef: 'targetRef_2',
+            },
+          ],
+        },
+        BPMNDiagram: {
+          id: 'BpmnDiagram_1',
+          BPMNPlane: {
+            id: 'BpmnPlane_1',
+            BPMNShape: {
+              id: 'shape_task_id_0',
+              bpmnElement: 'task_id_0',
+              Bounds: { x: 362, y: 232, width: 36, height: 45 },
+            },
+            BPMNEdge: [
+              {
+                id: 'edge_sequenceFlow_id_0',
+                bpmnElement: 'sequenceFlow_id_0',
+                waypoint: [{ x: 10, y: 10 }],
               },
-              "BPMNDiagram": {
-                  "id": "BpmnDiagram_1",
-                  "BPMNPlane": {
-                      "id": "BpmnPlane_1",
-                      "BPMNShape": {
-                          "id":"shape_task_id_0",
-                          "bpmnElement":"task_id_0",
-                          "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
-                      },
-                      "BPMNEdge": [{
-                            "id": "edge_sequenceFlow_id_0",
-                            "bpmnElement": "sequenceFlow_id_0"
-                        }, {
-                            "id": "edge_sequenceFlow_id_1",
-                            "bpmnElement": "sequenceFlow_id_1"
-                        }
-                      ]
-                  }
-              }
-          }
-      }`;
+              {
+                id: 'edge_sequenceFlow_id_1',
+                bpmnElement: 'sequenceFlow_id_1',
+                waypoint: [{ x: 10, y: 10 }],
+              },
+            ],
+          },
+        },
+      },
+    };
 
-    const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 2, 1);
+    // const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 2, 1);
+    const model = defaultBpmnJsonParser().parse(json);
+    expect(model.lanes).toHaveLength(0);
+    expect(model.pools).toHaveLength(0);
+    expect(model.flowNodes).toHaveLength(1);
+    expect(model.edges).toHaveLength(2);
 
     verifyEdge(model.edges[0], {
       edgeId: 'edge_sequenceFlow_id_0',
@@ -67,6 +83,7 @@ describe('parse bpmn as json for default sequence flow', () => {
       bpmnElementSourceRefId: 'task_id_0',
       bpmnElementTargetRefId: 'targetRef_RLk',
       bpmnElementSequenceFlowKind: SequenceFlowKind.DEFAULT,
+      waypoints: [new Waypoint(10, 10)],
     });
     verifyEdge(model.edges[1], {
       edgeId: 'edge_sequenceFlow_id_1',
@@ -75,6 +92,7 @@ describe('parse bpmn as json for default sequence flow', () => {
       bpmnElementSourceRefId: 'task_id_0',
       bpmnElementTargetRefId: 'targetRef_2',
       bpmnElementSequenceFlowKind: SequenceFlowKind.NORMAL,
+      waypoints: [new Waypoint(10, 10)],
     });
   });
 
@@ -98,36 +116,43 @@ describe('parse bpmn as json for default sequence flow', () => {
     // TODO: To uncomment when we support businessRuleTask
     //['businessRuleTask'],
   ]).it('json containing one process with a sequence flow defined as default in a %s', sourceKind => {
-    const json = `{
-          "definitions": {
-              "process": {
-                  "id": "Process_1",
-                  "${sourceKind}": { "id": "source_id_0", "default": "sequenceFlow_id_0"},
-                  "sequenceFlow": {
-                      "id": "sequenceFlow_id_0",
-                      "sourceRef": "source_id_0",
-                      "targetRef": "targetRef_RLk"
-                  }
-              },
-              "BPMNDiagram": {
-                  "id": "BpmnDiagram_1",
-                  "BPMNPlane": {
-                      "id": "BpmnPlane_1",
-                      "BPMNShape": {
-                          "id":"shape_source_id_0",
-                          "bpmnElement":"source_id_0",
-                          "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
-                      },
-                      "BPMNEdge": {
-                          "id": "edge_sequenceFlow_id_0",
-                          "bpmnElement": "sequenceFlow_id_0"
-                      }
-                  }
-              }
-          }
-      }`;
+    const json = {
+      definitions: {
+        targetNamespace: '',
+        process: {
+          id: 'Process_1',
+          sequenceFlow: {
+            id: 'sequenceFlow_id_0',
+            sourceRef: 'source_id_0',
+            targetRef: 'targetRef_RLk',
+          },
+        },
+        BPMNDiagram: {
+          id: 'BpmnDiagram_1',
+          BPMNPlane: {
+            id: 'BpmnPlane_1',
+            BPMNShape: {
+              id: 'shape_source_id_0',
+              bpmnElement: 'source_id_0',
+              Bounds: { x: 362, y: 232, width: 36, height: 45 },
+            },
+            BPMNEdge: {
+              id: 'edge_sequenceFlow_id_0',
+              bpmnElement: 'sequenceFlow_id_0',
+              waypoint: [{ x: 10, y: 10 }],
+            },
+          },
+        },
+      },
+    };
+    (json.definitions.process as TProcess)[`${sourceKind}`] = { id: 'source_id_0', default: 'sequenceFlow_id_0' };
 
-    const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 1, 1);
+    // const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 1, 1);
+    const model = defaultBpmnJsonParser().parse(json);
+    expect(model.lanes).toHaveLength(0);
+    expect(model.pools).toHaveLength(0);
+    expect(model.flowNodes).toHaveLength(1);
+    expect(model.edges).toHaveLength(1);
 
     verifyEdge(model.edges[0], {
       edgeId: 'edge_sequenceFlow_id_0',
@@ -136,40 +161,48 @@ describe('parse bpmn as json for default sequence flow', () => {
       bpmnElementSourceRefId: 'source_id_0',
       bpmnElementTargetRefId: 'targetRef_RLk',
       bpmnElementSequenceFlowKind: SequenceFlowKind.DEFAULT,
+      waypoints: [new Waypoint(10, 10)],
     });
   });
 
   it('json containing one process with a flow node who define a sequence flow as default, but not possible in BPMN Semantic', () => {
-    const json = `{
-          "definitions": {
-              "process": {
-                  "id": "Process_1",
-                  "parallelGateway": { "id": "gateway_id_0", "default": "sequenceFlow_id_0"},
-                  "sequenceFlow": {
-                      "id": "sequenceFlow_id_0",
-                      "sourceRef": "gateway_id_0",
-                      "targetRef": "targetRef_RLk"
-                  }
-              },
-              "BPMNDiagram": {
-                  "id": "BpmnDiagram_1",
-                  "BPMNPlane": {
-                      "id": "BpmnPlane_1",
-                      "BPMNShape": {
-                          "id":"shape_gateway_id_0",
-                          "bpmnElement":"gateway_id_0",
-                          "Bounds": { "x": 362, "y": 232, "width": 36, "height": 45 }
-                      },
-                      "BPMNEdge": {
-                          "id": "edge_sequenceFlow_id_0",
-                          "bpmnElement": "sequenceFlow_id_0"
-                      }
-                  }
-              }
-          }
-      }`;
+    const json = {
+      definitions: {
+        targetNamespace: '',
+        process: {
+          id: 'Process_1',
+          parallelGateway: { id: 'gateway_id_0', default: 'sequenceFlow_id_0' },
+          sequenceFlow: {
+            id: 'sequenceFlow_id_0',
+            sourceRef: 'gateway_id_0',
+            targetRef: 'targetRef_RLk',
+          },
+        },
+        BPMNDiagram: {
+          id: 'BpmnDiagram_1',
+          BPMNPlane: {
+            id: 'BpmnPlane_1',
+            BPMNShape: {
+              id: 'shape_gateway_id_0',
+              bpmnElement: 'gateway_id_0',
+              Bounds: { x: 362, y: 232, width: 36, height: 45 },
+            },
+            BPMNEdge: {
+              id: 'edge_sequenceFlow_id_0',
+              bpmnElement: 'sequenceFlow_id_0',
+              waypoint: [{ x: 10, y: 10 }],
+            },
+          },
+        },
+      },
+    };
 
-    const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 1, 1);
+    // const model = parseJsonAndExpectOnlyEdgesAndFlowNodes(json, 1, 1);
+    const model = defaultBpmnJsonParser().parse(json);
+    expect(model.lanes).toHaveLength(0);
+    expect(model.pools).toHaveLength(0);
+    expect(model.flowNodes).toHaveLength(1);
+    expect(model.edges).toHaveLength(1);
 
     verifyEdge(model.edges[0], {
       edgeId: 'edge_sequenceFlow_id_0',
@@ -178,6 +211,7 @@ describe('parse bpmn as json for default sequence flow', () => {
       bpmnElementSourceRefId: 'gateway_id_0',
       bpmnElementTargetRefId: 'targetRef_RLk',
       bpmnElementSequenceFlowKind: SequenceFlowKind.NORMAL,
+      waypoints: [new Waypoint(10, 10)],
     });
   });
 });
