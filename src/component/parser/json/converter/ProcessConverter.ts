2960524d2eaa5aab2f5c65660ebf3cb9163cd6a3
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-08-11 18:40:28 +0200

    simplify: find a way to remove the need for a dedicated subprocess kind

--- src/component/parser/json/converter/ProcessConverter.ts
+++ src/component/parser/json/converter/ProcessConverter.ts
@@ -21,7 +21,7 @@ import ShapeBpmnElement, {
   ShapeBpmnEvent,
   ShapeBpmnSubProcess,
 } from '../../../../model/bpmn/shape/ShapeBpmnElement';
-import { ShapeBpmnElementKind, SubProcessShapeBpmnElementKind, toShapeBpmnElementKind } from '../../../../model/bpmn/shape/ShapeBpmnElementKind';
+import { ShapeBpmnElementKind } from '../../../../model/bpmn/shape/ShapeBpmnElementKind';
 import { AssociationFlow, SequenceFlow } from '../../../../model/bpmn/edge/Flow';
 import { ShapeBpmnEventKind, supportedBpmnEventKinds } from '../../../../model/bpmn/shape/ShapeBpmnEventKind';
 import ShapeUtil, { BpmnEventKind } from '../../../../model/bpmn/shape/ShapeUtil';
@@ -248,18 +248,12 @@ export default class ProcessConverter {
   // TODO duplicated with the process part
   private buildSubprocessInnerElements(process: TSubProcess): void {
     // TODO manage laneset?
-
-    // const process = bpmnElement;
     const processId = process.id;
 
-    // MergeKind.
-
-    // type LogLevelStrings = SubProcessShapeBpmnElementKind.EVENT_START;
     // flow nodes
-    // ShapeUtil.flowNodeKinds()
-    Object.values(SubProcessShapeBpmnElementKind)
-      .filter(kind => kind != SubProcessShapeBpmnElementKind.EVENT_BOUNDARY)
-      .forEach(kind => this.buildFlowNodeBpmnElements(processId, process[kind], toShapeBpmnElementKind(kind)));
+    ShapeUtil.flowNodeKinds()
+      .filter(kind => kind != ShapeBpmnElementKind.EVENT_BOUNDARY)
+      .forEach(kind => this.buildFlowNodeBpmnElements(processId, process[kind], kind));
     // process boundary events afterwards as we need its parent activity to be available when building it
     this.buildFlowNodeBpmnElements(processId, process.boundaryEvent, ShapeBpmnElementKind.EVENT_BOUNDARY);
 
