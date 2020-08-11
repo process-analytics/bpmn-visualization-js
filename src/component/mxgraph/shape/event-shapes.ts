df5441b3a11f2c2f9283a8375672579aa4ab8bea
Céline Souchet <celine.souchet@bonitasoft.com>
2020-08-11 16:17:55 +0200

    Render Error End Event & Error Interrupting Boundary Event

--- src/component/mxgraph/shape/event-shapes.ts
+++ src/component/mxgraph/shape/event-shapes.ts
@@ -43,6 +43,10 @@ abstract class EventShape extends mxEllipse {
       ShapeBpmnEventKind.LINK,
       (paintParameter: PaintParameter) => this.iconPainter.paintRightArrowIcon({ ...paintParameter, ratioFromParent: 0.55, icon: { ...paintParameter.icon, strokeWidth: 1.5 } }),
     ],
+    [
+      ShapeBpmnEventKind.ERROR,
+      (paintParameter: PaintParameter) => this.iconPainter.paintErrorIcon({ ...paintParameter, ratioFromParent: 0.55, icon: { ...paintParameter.icon, strokeWidth: 1.5 } }),
+    ],
   ]);
 
   protected withFilledIcon = false;
@@ -61,11 +65,11 @@ abstract class EventShape extends mxEllipse {
   // This will be removed after implementation of all supported events
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   private markNonFullyRenderedEvents(c: mxAbstractCanvas2D): void {
-    const eventKind = StyleUtils.getBpmnEventKind(this.style);
-    if (eventKind == ShapeBpmnEventKind.ERROR) {
-      c.setFillColor('deeppink');
-      c.setFillAlpha(0.3);
-    }
+    // const eventKind = StyleUtils.getBpmnEventKind(this.style);
+    // if (eventKind == ShapeBpmnEventKind.ERROR) {
+    //   c.setFillColor('deeppink');
+    //   c.setFillAlpha(0.3);
+    // }
   }
 
   protected paintOuterShape({ c, shape: { x, y, w, h } }: PaintParameter): void {
