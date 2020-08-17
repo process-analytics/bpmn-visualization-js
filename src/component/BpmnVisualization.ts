758619609147dcab8e677a6195e4a97ae496d7ef
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-08-17 16:27:17 +0200

    MouseEvent configuration in a dedicated method (for clarity)

--- src/component/BpmnVisualization.ts
+++ src/component/BpmnVisualization.ts
@@ -36,88 +36,8 @@ export default class BpmnVisualization {
       // Instantiate and configure Graph
       const configurator = new MxGraphConfigurator(this.container);
 
-      // Changes the zoom / panning on mouseWheel events
       // TODO make activation/deactivation configurable
-      // eslint-disable-next-line @typescript-eslint/no-this-alias
-      const self = this; // TODO replace with array function to access to this directly
-      mxEvent.addMouseWheelListener(function (event: Event, up: boolean) {
-        if (!(event instanceof MouseEvent)) {
-          console.info('[MouseWheelListener] event is not a MouseEvent', event);
-          return;
-        }
-
-        // TODO review type: this hack is due to the introduction of mxgraph-type-definitions
-        const evt = (event as unknown) as MouseEvent;
-        const mxMouseEvent = (evt as unknown) as mxMouseEvent;
-        if (!mxEvent.isConsumed(mxMouseEvent)) {
-          // console.info('[MouseWheelListener] evt: up: %s / altkey: %s / ctrlKey: %s / shiftKey: %s', up, evt.altKey, evt.ctrlKey, evt.shiftKey);
-          // console.info('[MouseWheelListener] evt: x: %s / y: %s', evt.clientX, evt.clientY);
-          // console.info('MouseWheelListener mxMouseEvent: graphX: %s / graphY: %s', mxMouseEvent.graphX, mxMouseEvent.graphY);
-
-          let isEventRelatedToGraphContainer = false;
-          let source = mxEvent.getSource(evt);
-          console.info('[MouseWheelListener] Checking source');
-          while (source != null) {
-            // console.info('[MouseWheelListener] source', source);
-            if (source == self.graph.container) {
-              // console.info('[MouseWheelListener] is graph container!!');
-              isEventRelatedToGraphContainer = true;
-              break;
-            }
-            source = source.parentNode;
-          }
-          console.info('[MouseWheelListener] event related to the graph container?', isEventRelatedToGraphContainer);
-          if (!isEventRelatedToGraphContainer) {
-            return;
-          }
-
-          // only the ctrl key or the meta key on mac
-          const isZoomWheelEvent = (evt.ctrlKey || (mxClient.IS_MAC && evt.metaKey)) && !evt.altKey && !evt.shiftKey;
-
-          if (isZoomWheelEvent) {
-            console.info('[MouseWheelListener] zooming');
-            // TODO pass the point coordinate to use as center
-            const cursorPosition = new mxPoint(mxEvent.getClientX(evt), mxEvent.getClientY(evt));
-            // TODO we could have an option to ignoreCursorPosition
-            console.info('[MouseWheelListener] cursor position', cursorPosition);
-            self.zoom(up ? ZoomType.In : ZoomType.Out);
-            mxEvent.consume(evt);
-
-            // console.info('[MouseWheelListener] after zoom - scrolling to cursorPosition');
-            // // https://github.com/jgraph/drawio/blob/051eed36389f5bebe487663097f340e59940d87a/src/main/webapp/js/mxgraph/EditorUi.js#L2216
-            // // const sp = new mxPoint(self.graph.container.scrollLeft, self.graph.container.scrollTop);
-            // const offset = mxUtils.getOffset(self.graph.container);
-            // console.info('[MouseWheelListener] after zoom - offset', offset);
-            // // const prev = self.graph.view.scale;
-            // let dx = 0;
-            // let dy = 0;
-            //
-            // dx = self.graph.container.offsetWidth / 2 - cursorPosition.x + offset.x;
-            // dy = self.graph.container.offsetHeight / 2 - cursorPosition.y + offset.y;
-            // console.info('[MouseWheelListener] after zoom - dx', dx);
-            // console.info('[MouseWheelListener] after zoom - dy', dy);
-            //
-            // self.graph.container.scrollLeft -= dx;
-            // self.graph.container.scrollTop -= dy;
-            // console.info('[MouseWheelListener] after zoom - scrolling done');
-          }
-          // shift only
-          const isPanningHorizontalWheelEvent = evt.shiftKey && !evt.altKey && !evt.ctrlKey && !evt.metaKey;
-          // no key
-          const isPanningVerticalWheelEvent = !evt.altKey && !evt.shiftKey && !evt.ctrlKey && !evt.metaKey;
-
-          if (isPanningHorizontalWheelEvent || isPanningVerticalWheelEvent) {
-            let panType: PanType;
-            if (isPanningHorizontalWheelEvent) {
-              panType = up ? PanType.HorizontalLeft : PanType.HorizontalRight;
-            } else {
-              panType = up ? PanType.VerticalUp : PanType.VerticalDown;
-            }
-            self.pan(panType);
-            mxEvent.consume(evt);
-          }
-        }
-      }, null);
+      this.configureMouseEvent(false);
 
       this.graph = configurator.configure(options);
       this.configureKeyHandler();
@@ -126,6 +46,95 @@ export default class BpmnVisualization {
       mxUtils.alert('Cannot start application: ' + e.message);
       throw e;
     }
+  }
+
+  // Changes the zoom / panning on mouseWheel events
+  // TODO this should be done in another class
+  private configureMouseEvent(activated: boolean): void {
+    if (!activated) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const self = this; // TODO replace with array function to access to this directly
+    mxEvent.addMouseWheelListener(function (event: Event, up: boolean) {
+      if (!(event instanceof MouseEvent)) {
+        console.info('[MouseWheelListener] event is not a MouseEvent', event);
+        return;
+      }
+
+      // TODO review type: this hack is due to the introduction of mxgraph-type-definitions
+      const evt = (event as unknown) as MouseEvent;
+      const mxMouseEvent = (evt as unknown) as mxMouseEvent;
+      if (!mxEvent.isConsumed(mxMouseEvent)) {
+        // console.info('[MouseWheelListener] evt: up: %s / altkey: %s / ctrlKey: %s / shiftKey: %s', up, evt.altKey, evt.ctrlKey, evt.shiftKey);
+        // console.info('[MouseWheelListener] evt: x: %s / y: %s', evt.clientX, evt.clientY);
+        // console.info('MouseWheelListener mxMouseEvent: graphX: %s / graphY: %s', mxMouseEvent.graphX, mxMouseEvent.graphY);
+
+        let isEventRelatedToGraphContainer = false;
+        let source = mxEvent.getSource(evt);
+        console.info('[MouseWheelListener] Checking source');
+        while (source != null) {
+          // console.info('[MouseWheelListener] source', source);
+          if (source == self.graph.container) {
+            // console.info('[MouseWheelListener] is graph container!!');
+            isEventRelatedToGraphContainer = true;
+            break;
+          }
+          source = source.parentNode;
+        }
+        console.info('[MouseWheelListener] event related to the graph container?', isEventRelatedToGraphContainer);
+        if (!isEventRelatedToGraphContainer) {
+          return;
+        }
+
+        // only the ctrl key or the meta key on mac
+        const isZoomWheelEvent = (evt.ctrlKey || (mxClient.IS_MAC && evt.metaKey)) && !evt.altKey && !evt.shiftKey;
+
+        if (isZoomWheelEvent) {
+          console.info('[MouseWheelListener] zooming');
+          // TODO pass the point coordinate to use as center
+          const cursorPosition = new mxPoint(mxEvent.getClientX(evt), mxEvent.getClientY(evt));
+          // TODO we could have an option to ignoreCursorPosition
+          console.info('[MouseWheelListener] cursor position', cursorPosition);
+          self.zoom(up ? ZoomType.In : ZoomType.Out);
+          mxEvent.consume(evt);
+
+          // console.info('[MouseWheelListener] after zoom - scrolling to cursorPosition');
+          // // https://github.com/jgraph/drawio/blob/051eed36389f5bebe487663097f340e59940d87a/src/main/webapp/js/mxgraph/EditorUi.js#L2216
+          // // const sp = new mxPoint(self.graph.container.scrollLeft, self.graph.container.scrollTop);
+          // const offset = mxUtils.getOffset(self.graph.container);
+          // console.info('[MouseWheelListener] after zoom - offset', offset);
+          // // const prev = self.graph.view.scale;
+          // let dx = 0;
+          // let dy = 0;
+          //
+          // dx = self.graph.container.offsetWidth / 2 - cursorPosition.x + offset.x;
+          // dy = self.graph.container.offsetHeight / 2 - cursorPosition.y + offset.y;
+          // console.info('[MouseWheelListener] after zoom - dx', dx);
+          // console.info('[MouseWheelListener] after zoom - dy', dy);
+          //
+          // self.graph.container.scrollLeft -= dx;
+          // self.graph.container.scrollTop -= dy;
+          // console.info('[MouseWheelListener] after zoom - scrolling done');
+        }
+        // shift only
+        const isPanningHorizontalWheelEvent = evt.shiftKey && !evt.altKey && !evt.ctrlKey && !evt.metaKey;
+        // no key
+        const isPanningVerticalWheelEvent = !evt.altKey && !evt.shiftKey && !evt.ctrlKey && !evt.metaKey;
+
+        if (isPanningHorizontalWheelEvent || isPanningVerticalWheelEvent) {
+          let panType: PanType;
+          if (isPanningHorizontalWheelEvent) {
+            panType = up ? PanType.HorizontalLeft : PanType.HorizontalRight;
+          } else {
+            panType = up ? PanType.VerticalUp : PanType.VerticalDown;
+          }
+          self.pan(panType);
+          mxEvent.consume(evt);
+        }
+      }
+    }, null);
   }
 
   private keyHandlerLogger = new Logger('KeyHandler');
