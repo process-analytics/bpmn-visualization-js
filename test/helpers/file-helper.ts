36d1d030597c1799241c155b133fa691f048fa5a
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-09-17 13:31:16 +0200

    [POC] cli tool to parse files .bpmn into json (logged in console)

--- test/helpers/file-helper.ts
+++ test/helpers/file-helper.ts
@@ -18,6 +18,7 @@ import { join } from 'path';
 import debugLogger from 'debug';
 
 const debug = debugLogger('test');
+const __dirname = path.resolve();
 
 export function readFileSync(relPathToSourceFile: string, encoding = 'utf8', dirName = __dirname): string {
   return fsReadFileSync(join(dirName, relPathToSourceFile), encoding);
---
+++ test/scripts/parseJson.ts
@@ -0,0 +1,29 @@
+import BpmnXmlParser from '../../src/component/parser/xml/BpmnXmlParser';
+import { readFileSync } from '../helpers/file-helper';
+
+/**
+ * Copyright 2020 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const myArgs = process.argv.slice(2);
+const bpmnFilePath = myArgs[0];
+if (!bpmnFilePath) {
+  throw new Error('you must provide file path as parameter');
+}
+
+const xmlParser = new BpmnXmlParser();
+const json = xmlParser.parse(readFileSync(bpmnFilePath));
+
+// eslint-disable-next-line no-console
+console.log(JSON.stringify(json, null, 2));
