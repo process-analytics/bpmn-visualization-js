14c9c2df900dec82a0fe7b28357cf1289231639e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-09-26 19:36:44 +0200

    wip: generalize test

--- test/unit/component/mxgraph/shape/render/utils.test.ts
+++ test/unit/component/mxgraph/shape/render/utils.test.ts
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { orderActivityMarkers } from '../../../../../../src/component/mxgraph/shape/render/utils';
 import { ShapeBpmnMarkerKind } from '../../../../../../src/model/bpmn/shape';
-import { TProcess } from '../../../../../../src/component/parser/xml/bpmn-json-model/baseElement/rootElement/rootElement';
 
 describe('enforce activity markers order', () => {
   describe('1 element', () => {
@@ -26,35 +24,34 @@ describe('enforce activity markers order', () => {
     });
   });
 
-  // BPM rules: they cannot at the same time
-  const incompatibleElements = [ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL, ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL];
-
-  // adhoc: only with expand and compensation
-
   describe('2 elements', () => {
-    const input1: string[] = [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.LOOP];
-    it(`2 elements - order: ${input1}`, () => {
-      expect(orderActivityMarkers(input1)).toEqual([ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND]);
-    });
-    const input2: string[] = [ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND];
-    it(`2 elements - order: ${input2}`, () => {
-      expect(orderActivityMarkers(input2)).toEqual([ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND]);
-    });
-    const input3: string[] = [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL];
-    it(`2 elements - order: ${input3}`, () => {
-      expect(orderActivityMarkers(input3)).toEqual([ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL, ShapeBpmnMarkerKind.EXPAND]);
-    });
-    const input4: string[] = [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL];
-    it(`2 elements - order: ${input4}`, () => {
-      expect(orderActivityMarkers(input4)).toEqual([ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, ShapeBpmnMarkerKind.EXPAND]);
-    });
-    const input5: string[] = [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.COMPENSATION];
-    it(`order: ${input5}`, () => {
-      expect(orderActivityMarkers(input5)).toEqual([ShapeBpmnMarkerKind.COMPENSATION, ShapeBpmnMarkerKind.EXPAND]);
-    });
-    const input6: string[] = [ShapeBpmnMarkerKind.COMPENSATION, ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL];
-    it(`order: ${input6}`, () => {
-      expect(orderActivityMarkers(input6)).toEqual([ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, ShapeBpmnMarkerKind.COMPENSATION]);
+    it.each([
+      [
+        [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.LOOP],
+        [ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND],
+      ],
+      [
+        [ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND],
+        [ShapeBpmnMarkerKind.LOOP, ShapeBpmnMarkerKind.EXPAND],
+      ],
+      [
+        [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL],
+        [ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL, ShapeBpmnMarkerKind.EXPAND],
+      ],
+      [
+        [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL],
+        [ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, ShapeBpmnMarkerKind.EXPAND],
+      ],
+      [
+        [ShapeBpmnMarkerKind.EXPAND, ShapeBpmnMarkerKind.COMPENSATION],
+        [ShapeBpmnMarkerKind.COMPENSATION, ShapeBpmnMarkerKind.EXPAND],
+      ],
+      [
+        [ShapeBpmnMarkerKind.COMPENSATION, ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL],
+        [ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, ShapeBpmnMarkerKind.COMPENSATION],
+      ],
+    ])(`order: %s)`, (markers: string[], expectedOrderedMarkers: string[]) => {
+      expect(orderActivityMarkers(markers)).toEqual(expectedOrderedMarkers);
     });
   });
 
@@ -89,7 +86,7 @@ describe('enforce activity markers order', () => {
     });
   });
 
-  // To support extensions that add markers
+  // Support extensions that add markers
   describe('extra elements', () => {
     it.each([
       [
