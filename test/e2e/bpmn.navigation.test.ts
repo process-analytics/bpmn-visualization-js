cea8659868a821ae38a29bccd46cf2145d80761e
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-10-13 18:23:58 +0200

    add tests for zoom (zoom in, zoom out)

--- test/e2e/bpmn.navigation.test.ts
+++ test/e2e/bpmn.navigation.test.ts
@@ -14,8 +14,31 @@
  * limitations under the License.
  */
 import { BpmnDiagramPreparation, ImageSnapshotConfigurator, PageTester } from './helpers/visu-utils';
+import { Mouse } from 'puppeteer';
+// FIXME - to be fixed when new release of puppeteer comes out
+// wheel is added in version @types/puppeteer 2.1.5 but for some reason not in 3.0.2
+// perhaps will be soon available in 3.0.3
+interface MouseWheelOptions {
+  /**
+   * X delta in CSS pixels for mouse wheel event (default: 0). Positive values emulate a scroll up and negative values a scroll down event.
+   * @default 0
+   */
+  deltaX?: number;
+  /**
+   *  Y delta in CSS pixels for mouse wheel event (default: 0). Positive values emulate a scroll right and negative values a scroll left event.
+   * @default 0
+   */
+  deltaY?: number;
+}
+interface MouseWithWheel extends Mouse {
+  /**
+   * Dispatches a `mousewheel` event.
+   * @param options The mouse wheel options.
+   */
+  wheel(options?: MouseWheelOptions): Promise<void>;
+}
 
-describe('diagram navigation', () => {
+describe('diagram navigation', async () => {
   const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new Map());
 
   // to have mouse pointer visible during headless test - add 'showMousePointer=true' to queryParams
@@ -23,19 +46,29 @@ describe('diagram navigation', () => {
 
   const pageTester = new PageTester(bpmnDiagramPreparation, 'bpmn-viewport', 'BPMN Visualization - Diagram Navigation');
 
+  const fileName = 'simple-2_start_events-1_task';
+  const bpmnViewportElementHandle = await pageTester.expectBpmnDiagramToBeDisplayed(fileName);
+  const bounding_box = await bpmnViewportElementHandle.boundingBox();
+  const viewportCenterX = bounding_box.x + bounding_box.width / 2;
+  const viewportCenterY = bounding_box.y + bounding_box.height / 2;
+
   it('mouse panning', async () => {
-    const fileName = 'simple-2_start_events-1_task';
-    const bpmnViewportElementHandle = await pageTester.expectBpmnDiagramToBeDisplayed(fileName);
-
-    const bounding_box = await bpmnViewportElementHandle.boundingBox();
-
     // simulate mouse panning
-    const viewportCenterX = bounding_box.x + bounding_box.width / 2;
-    const viewportCenterY = bounding_box.y + bounding_box.height / 2;
     await page.mouse.move(viewportCenterX, viewportCenterY);
     await page.mouse.down();
     await page.mouse.move(viewportCenterX + 150, viewportCenterY + 40);
     await page.mouse.up();
+
+    const image = await page.screenshot({ fullPage: true });
+    expect(image).toMatchImageSnapshot(imageSnapshotConfigurator.getConfig(fileName));
+  });
+
+  it.each(['zoom in', 'zoom out'])(`ctrl + mouse: %s`, async (zoom: string) => {
+    const deltaX = zoom === 'zoom in' ? -100 : 100;
+    // simulate mouse+ctrl zoom
+    await page.mouse.move(viewportCenterX, viewportCenterY);
+    await page.keyboard.down('Control');
+    await (<MouseWithWheel>page.mouse).wheel({ deltaX: deltaX });
 
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot(imageSnapshotConfigurator.getConfig(fileName));
