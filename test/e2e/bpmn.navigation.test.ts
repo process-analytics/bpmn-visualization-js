50f5fb43e72b62541dc47c566f0d48ae45629b00
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-10-13 10:02:35 +0200

    Use dedicated bpmn diagram for navigation tests

--- test/e2e/bpmn.navigation.test.ts
+++ test/e2e/bpmn.navigation.test.ts
@@ -19,23 +19,22 @@ describe('diagram navigation', () => {
   const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new Map());
 
   // to have mouse pointer visible during headless test - add 'showMousePointer=true' to queryParams
-  const bpmnDiagramPreparation = new BpmnDiagramPreparation(new Map(), { name: 'navigation-diagram', queryParams: [] }, 'non-regression');
+  const bpmnDiagramPreparation = new BpmnDiagramPreparation(new Map(), { name: 'navigation-diagram', queryParams: [] }, 'navigation');
 
   const pageTester = new PageTester(bpmnDiagramPreparation, 'bpmn-viewport', 'BPMN Visualization - Diagram Navigation');
 
   it('mouse panning', async () => {
-    const fileName = 'gateways';
+    const fileName = 'simple-2_start_events-1_task';
     const bpmnViewportElementHandle = await pageTester.expectBpmnDiagramToBeDisplayed(fileName);
 
     const bounding_box = await bpmnViewportElementHandle.boundingBox();
 
     // simulate mouse panning
-    const moveThreshold = 30;
     const viewportCenterX = bounding_box.x + bounding_box.width / 2;
     const viewportCenterY = bounding_box.y + bounding_box.height / 2;
     await page.mouse.move(viewportCenterX, viewportCenterY);
     await page.mouse.down();
-    await page.mouse.move(viewportCenterX + moveThreshold, viewportCenterY + moveThreshold);
+    await page.mouse.move(viewportCenterX + 150, viewportCenterY + 40);
     await page.mouse.up();
 
     const image = await page.screenshot({ fullPage: true });
