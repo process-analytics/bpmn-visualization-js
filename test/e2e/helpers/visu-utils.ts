21f15797c2553a477c213fd44b78d39f6abccc5b
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-10-28 17:34:54 +0100

    [TEST] performance zoom

--- test/e2e/helpers/visu-utils.ts
+++ test/e2e/helpers/visu-utils.ts
@@ -183,3 +183,9 @@ export class PageTester {
     return bpmnViewportElementHandle;
   }
 }
+
+export function delay(time: number): Promise<unknown> {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
