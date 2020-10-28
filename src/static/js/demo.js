d5e6f45bbde01d0b80a3bda7df9a6fd74b0b0709
Céline Souchet <celine.souchet@bonitasoft.com>
2020-10-28 14:07:11 +0100

    Fix hidden control panel

--- src/static/js/demo.js
+++ src/static/js/demo.js
@@ -16,18 +16,6 @@
 import { documentReady, handleFileSelect, startBpmnVisualization, FitType, updateFitType } from '../../index.es.js';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function startDemo() {
-  startBpmnVisualization({ container: 'graph', loadOptions: { fitType: FitType[fitTypeSelected.value] } });
-  document.getElementById('bpmn-file').addEventListener('change', handleFileSelect, false);
-  fitTypeSelected.addEventListener('change', updateFitTypeSelection, false);
-
-  const parameters = new URLSearchParams(window.location.search);
-  if (!(parameters.get('hideControls') === 'true')) {
-    document.getElementById('controls').classList.remove('hidden');
-  }
-}
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function updateFitTypeSelection(event) {
   updateFitType(event);
 
@@ -50,17 +38,32 @@ function resetClass(htmlElementId) {
   htmlElement.classList.remove('fixed-size');
 }
 
-// Update the selected option at the initialization
-const parameters = new URLSearchParams(window.location.search);
-const parameterFitType = parameters.get('fitType');
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function startDemo() {
+  const parameters = new URLSearchParams(window.location.search);
 
-const fitTypeSelected = document.getElementById('fitType-selected');
-if (parameterFitType) {
-  fitTypeSelected.value = parameterFitType;
-}
+  // Update the selected option at the initialization
+  const fitTypeSelected = document.getElementById('fitType-selected');
+  fitTypeSelected.addEventListener('change', updateFitTypeSelection, false);
 
-if (fitTypeSelected.value !== 'None') {
-  setFixedSizeClass('graph');
+  const parameterFitType = parameters.get('fitType');
+  if (parameterFitType) {
+    fitTypeSelected.value = parameterFitType;
+  }
+
+  if (fitTypeSelected.value !== 'None') {
+    setFixedSizeClass('graph');
+  }
+
+  startBpmnVisualization({ container: 'graph', loadOptions: { fitType: FitType[fitTypeSelected.value] } });
+  document.getElementById('bpmn-file').addEventListener('change', handleFileSelect, false);
+
+  // Update control panel
+  if (parameters.get('hideControls') === 'true') {
+    const classList = document.getElementById('controls').classList;
+    classList.remove('controls');
+    classList.add('hidden');
+  }
 }
 
 // Start
