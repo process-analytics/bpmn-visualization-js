1a371f1f2c6d5bc14b9db88c120cbeb2a66e96dd
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-11-03 14:15:34 +0100

    Revert "simplify undefined check"
    This reverts commit b20189f3
    
    The commit was making TSC fail.

--- src/component/mxgraph/BpmnMxGraph.ts
+++ src/component/mxgraph/BpmnMxGraph.ts
@@ -31,7 +31,7 @@ export class BpmnMxGraph extends mxGraph {
 
   public customFit(fitOptions: FitOptions): void {
     const type = fitOptions?.type;
-    if (!type || type == FitType.None) {
+    if (type == undefined || type == FitType.None) {
       return;
     }
 
