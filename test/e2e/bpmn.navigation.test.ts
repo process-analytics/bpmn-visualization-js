6cafe1e0cb55dcd12ee83fa0143c2bd6799259b7
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-11-09 09:51:30 +0100

    try to fix mac tests

--- test/e2e/bpmn.navigation.test.ts
+++ test/e2e/bpmn.navigation.test.ts
@@ -77,14 +77,12 @@ describe('diagram navigation', () => {
     await page.keyboard.down('Control');
     for (let i = 0; i < xTimes; i++) {
       await page.mouse.wheel({ deltaX: deltaX });
-      await delay(50);
     }
-    await delay(100);
+    await delay(300);
     for (let i = 0; i < xTimes; i++) {
       await page.mouse.wheel({ deltaX: -deltaX });
-      await delay(50);
     }
-    await delay(100);
+    await delay(300);
     const image = await page.screenshot({ fullPage: true });
     expect(image).toMatchImageSnapshot(imageSnapshotConfigurator.getConfig(fileName));
   });
