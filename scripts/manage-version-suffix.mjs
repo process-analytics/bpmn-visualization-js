6d13042f137722a739159f9f8ef14d771147e658
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2020-12-08 07:06:40 +0100

    [INFRA] manage development version and align versions in config files
    We now align versions in npm and sonar config files at release time.
    So the SonarCloud displays the correct version without us having to maintain
    the version manually.
    
    After a new release has been done, we now set the project version with a
    dedicated suffix to clearly state that the master branch contains a development
    version.
    About the implementation
      - it was not possible to use the 'preversion' npm version script otherwise,
      npm would have considered the suffixed version to compute the new one. So, we
      use 2 npm calls when performing the version bump.
      - the suffix removal commit command uses a --no-verify flag to avoid a warning
       generated by husky. This should not cause issue as we don't need linting on
       the files we modified.

---
+++ scripts/manage-version-suffix.mjs
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2020 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as fs from 'fs';
+
+// const json = fs.readFileSync('./package.json', 'utf8').toString();
+// const pkg = JSON.parse(json);
+// const oldVersion = pkg.version;
+// const version = oldVersion.replace(/-post$/, '');
+//
+// const updated = json.replace(`"version": "${oldVersion}"`, `"version": "${version}"`);
+// fs.writeFileSync('./package.json', updated);
+
+log('Managing version suffix in configuration files');
+const currentVersion = getCurrentVersion();
+log('Current version', currentVersion);
+
+const newVersion = addOrRemoveVersionSuffix(currentVersion);
+log('New version', newVersion);
+
+updateVersionInNpmFile('./package.json', currentVersion, newVersion);
+updateVersionInNpmFile('./package-lock.json', currentVersion, newVersion);
+updateVersionInSonarFile('./sonar-project.properties', currentVersion, newVersion);
+
+log('Configuration files have been updated');
+
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+function log(...data) {
+  // eslint-disable-next-line no-console
+  console.info(...data);
+}
+
+function getCurrentVersion() {
+  const json = fs.readFileSync('./package.json', 'utf8').toString();
+  const pkg = JSON.parse(json);
+  return pkg.version;
+}
+
+function addOrRemoveVersionSuffix(version) {
+  return version.endsWith('-post') ? version.replace(/-post$/, '') : `${version}-post`;
+}
+
+function updateVersionInNpmFile(path, currentVersion, newVersion) {
+  const content = fs.readFileSync(path, 'utf8').toString();
+  const updatedContent = content.replace(`"version": "${currentVersion}"`, `"version": "${newVersion}"`);
+  fs.writeFileSync(path, updatedContent);
+}
+
+function updateVersionInSonarFile(path, currentVersion, newVersion) {
+  const content = fs.readFileSync(path, 'utf8').toString();
+  const updatedContent = content.replace(`sonar.projectVersion=${currentVersion}`, `sonar.projectVersion=${newVersion}`);
+  fs.writeFileSync(path, updatedContent);
+}
