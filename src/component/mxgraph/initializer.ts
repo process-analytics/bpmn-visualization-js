4562e2e325833b762ec87af80eab7b766f067e2a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-01-19 01:09:26 +0100

    disable eslint for any when setting mxgraph global options on the window object

--- src/component/mxgraph/initializer.ts
+++ src/component/mxgraph/initializer.ts
@@ -18,14 +18,16 @@ import factory, { mxGraphExportObject } from 'mxgraph';
 export const mxgraph = initialize();
 
 function initialize(): mxGraphExportObject {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   // set options globally, as it is not working when passing options to the factory (https://github.com/jgraph/mxgraph/issues/479)
   (window as any)['mxLoadResources'] = false;
   (window as any)['mxLoadStylesheets'] = false;
-  // extras, otherwise we got 'Uncaught ReferenceError: assignment to undeclared variable mxForceIncludes'
+  // extras, otherwise we got 'Uncaught ReferenceError: assignment to undeclared variable mx...'
   (window as any)['mxForceIncludes'] = false;
   (window as any)['mxResourceExtension'] = '.txt';
-  //return factory({});
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 
+  //return factory({});
   const mxgraphLib = factory({});
   // eslint-disable-next-line no-console
   console.info(`mxGraph ${mxgraphLib.mxClient.VERSION} initialized`);
