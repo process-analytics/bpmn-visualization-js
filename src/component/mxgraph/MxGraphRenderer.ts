376bd5af2360789226040c6f4429bd503083025b
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-01-19 01:03:37 +0100

    remove unused import after rebase on master

--- src/component/mxgraph/MxGraphRenderer.ts
+++ src/component/mxgraph/MxGraphRenderer.ts
@@ -27,7 +27,7 @@ import { MessageVisibleKind } from '../../model/bpmn/internal/edge/MessageVisibl
 import { ShapeBpmnMarkerKind } from '../../model/bpmn/internal/shape';
 import { BpmnMxGraph } from './BpmnMxGraph';
 import { LoadOptions } from '../options';
-import { mxCell, mxPoint } from 'mxgraph'; // for types
+import { mxCell } from 'mxgraph'; // for types
 import { mxgraph } from './initializer';
 
 export default class MxGraphRenderer {
