2aee22e4838f9b70fa7409d779067a3aa1b720c9
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-01-22 15:08:07 +0100

    Refactor test: move get css classes tests in a dedicated describe function

--- test/unit/component/registry/css-registry.test.ts
+++ test/unit/component/registry/css-registry.test.ts
@@ -16,6 +16,18 @@
 import { CssRegistry } from '../../../../src/component/registry/css-registry';
 
 describe('manage css classes for BPMN cells', () => {
+  describe('Get css classes', () => {
+    let cssRegistry: CssRegistry;
+    beforeEach(() => {
+      cssRegistry = new CssRegistry();
+    });
+
+    it('getClassNames should return a empty array, when no class name is registered for the BPMN element', () => {
+      cssRegistry.addClassNames('bpmn-id-1', ['class-name']);
+      expect(cssRegistry.getClassNames('bpmn-id-2')).toHaveLength(0);
+    });
+  });
+
   describe('Add css classes', () => {
     let cssRegistry: CssRegistry;
     beforeEach(() => {
@@ -60,12 +72,6 @@ describe('manage css classes for BPMN cells', () => {
 
       expect(result).toBeTruthy();
       expect(cssRegistry.getClassNames(bpmnElementId)).toEqual(['class-name-1', 'class-name-2', 'class-name-3', 'class-name-4']);
-    });
-
-    it('getClassNames should return a empty array, when no class name is registered for the BPMN element', () => {
-      cssRegistry.addClassNames('bpmn-id-1', ['class-name']);
-
-      expect(cssRegistry.getClassNames('bpmn-id-2')).toHaveLength(0);
     });
   });
 
