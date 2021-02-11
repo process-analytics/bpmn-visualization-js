24efb7d5c997138caf36732e197977babcd373ba
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-02-11 18:06:24 +0100

    perf test: simplify jest-playwright configuration

--- test/performance/jest.config.js
+++ test/performance/jest.config.js
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+process.env.JEST_PLAYWRIGHT_CONFIG = './test/performance/jest-playwright.config.js';
+
 module.exports = {
   rootDir: '../..',
   roots: ['./test/performance', './src'],
