136fbdc1cda4caac2d1e8a00083391483aa9e25d
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2021-05-03 16:40:37 +0200

    thresholds

--- test/e2e/overlays.rendering.test.ts
+++ test/e2e/overlays.rendering.test.ts
@@ -83,7 +83,7 @@ class ImageSnapshotThresholds extends MultiBrowserImageSnapshotThresholds {
       [
         'overlays.edges.message.flows.complex.paths',
         {
-          linux: 0.0032, // 0.31451721857130854%
+          linux: 0.0039, // 0.38563259095634184%
           macos: 0.004, // 0.36700887485542344%
         },
       ],
