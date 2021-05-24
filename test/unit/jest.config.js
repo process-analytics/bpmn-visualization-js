44823fb318bc08fa839db1a835cc393dff932334
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2021-05-24 10:19:37 +0200

    Fix 'src/static' path usage in configuration files

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -17,12 +17,12 @@ module.exports = {
   rootDir: '../..',
   roots: ['./test/unit', './src'],
   testMatch: ['**/?(*.)+(spec|test).[t]s'],
-  testPathIgnorePatterns: ['/node_modules/', 'dist', 'src'],
+  testPathIgnorePatterns: ['/node_modules/', 'dev', 'dist', 'src'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.{ts,js}'],
-  coveragePathIgnorePatterns: ['/node_modules/', 'dist', 'test', 'src/demo', 'src/static', 'src/model'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'dev', 'dist', 'src/demo', 'src/model', 'test'],
   coverageReporters: ['lcov', 'text-summary'],
   coverageDirectory: 'build/test-report/unit',
   reporters: [
