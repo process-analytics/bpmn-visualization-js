d022722263762138436c0f7e88c0484849894d1e
Souchet Céline <celine.souchet@bonitasoft.com>
2021-12-14 18:36:52 +0100

    Fix test

--- test/unit/component/parser/json/BpmnJsonParser.process.test.ts
+++ test/unit/component/parser/json/BpmnJsonParser.process.test.ts
@@ -773,6 +773,6 @@ describe('parse bpmn as json for process/pool', () => {
 
     const model = parseJsonAndExpect(json, 0, 0, 5, 4);
 
-    model.flowNodes.map(flowNode => flowNode.bpmnElement).forEach(bpmnElement => expect(bpmnElement.parentId).toBeUndefined());
+    model.flowNodes.map(flowNode => flowNode.bpmnElement).forEach(bpmnElement => expect(bpmnElement.parentId).toBe('WFP-6-'));
   });
 });
