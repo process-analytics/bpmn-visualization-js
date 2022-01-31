4f7da011f3c6c479ed9ac7706239667c6cb60afa
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-01-31 11:12:49 +0100

    [TEST] Improve the e2e test reports
    Implement a jest-image-snapshot custom matcher to provide more information on
    error. It makes the debug easier.
      - Generate expected and actual images along with the already existing diff
      image. We can then make the diff with these images with other tools
      - Display the configured 'failureThreshold' in the error message for
      comparison with the actual diff percentage.
    
    It could be integrated directly in jest-image-snapshot, but currently, the
    project seems inactive and doesn't merge PR. So do it here for now. We will
    propose the change later.

--- test/config/jest.image.ts
+++ test/config/jest.image.ts
@@ -13,6 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import type { MatchImageSnapshotOptions } from 'jest-image-snapshot';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import MatcherContext = jest.MatcherContext;
+import CustomMatcherResult = jest.CustomMatcherResult;
+import { writeFileSync, copyFileSync } from 'fs';
 
-expect.extend({ toMatchImageSnapshot });
+const toMatchImageSnapshotWithRealSignature = toMatchImageSnapshot as (received: unknown, options?: MatchImageSnapshotOptions) => CustomMatcherResult;
+
+// Improve jest-image-snapshot outputs to facilitate debug
+// The 'options' parameter is mandatory for us, and some properties must be set as well
+// If the following implementation would be done directly in jest-image-snapshot, this won't be required as it set default values we cannot access here
+function toMatchImageSnapshotCustom(this: MatcherContext, received: Buffer, options: MatchImageSnapshotOptions): CustomMatcherResult {
+  const result = toMatchImageSnapshotWithRealSignature.call(this, received, options);
+  if (!result.pass) {
+    // Generate expected and actual images
+    // the following options properties are always set in bpmn-visualization tests
+    const originalImagePath = `${options.customSnapshotsDir}/${options.customSnapshotIdentifier}-snap.png`;
+    const expectedImagePath = `${options.customDiffDir}/${options.customSnapshotIdentifier}-diff-01-expected.png`;
+    copyFileSync(originalImagePath, expectedImagePath);
+    const actualImagePath = `${options.customDiffDir}/${options.customSnapshotIdentifier}-diff-02-actual.png`;
+    writeFileSync(actualImagePath, received);
+
+    // Add configured failure threshold in the error message
+    const messages = result.message().split('\n');
+    // For generalization, check options.failureThresholdType for percentage/pixel
+    const newMessage = [`${messages[0]} Failure threshold was set to ${options.failureThreshold * 100}%.`, ...messages.slice(1)].join('\n');
+    result.message = () => newMessage;
+  }
+
+  return result;
+}
+
+expect.extend({ toMatchImageSnapshot: toMatchImageSnapshotCustom });
