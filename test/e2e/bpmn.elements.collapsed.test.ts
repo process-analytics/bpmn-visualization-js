8fdb130178201113f0a65b397a1d18cd0854bcf5
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-03-16 11:35:20 +0100

    [TEST] Add visual tests for edges targeting collapsed elements
    Changes
      - add diagrams for expanded/collapsed pools and subprocesses
      - HTML page: manage collapse element query parameter
      - added tests: test the diagram rendering with and without collapsed elements
    
    Note that collapsing elements is not officially supported and is managed with mxGraph custom code.
    We demonstrate it in the examples, so the new tests ensure the sequence and message flows are rendered
    correctly in this case.

---
+++ test/e2e/bpmn.elements.collapsed.test.ts
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2022 Bonitasoft S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { MatchImageSnapshotOptions } from 'jest-image-snapshot';
+import { ImageSnapshotConfigurator, MultiBrowserImageSnapshotThresholds } from './helpers/visu/image-snapshot-config';
+import { PageTester } from './helpers/visu/bpmn-page-utils';
+import type { Page } from 'playwright';
+import { getBpmnDiagramNames } from './helpers/test-utils';
+
+// key: diagram name
+// values: the ids of the elements to collapse. The elements are collapsed one by one, in dedicated tests
+const elementsToCollapsePerDiagram = new Map<string, Array<string>>([
+  ['pools', ['Participant_1', 'Participant_2', 'Participant_3', 'Participant_4']],
+  ['subprocess', ['SubProcess_1']],
+]);
+
+class CollapsedElementImageSnapshotConfigurator extends ImageSnapshotConfigurator {
+  override getConfig(param: { fileName: string; collapsedElement: string }): MatchImageSnapshotOptions {
+    const config = super.getConfig(param);
+    config.customSnapshotIdentifier = `${param.fileName}-collapse-${param.collapsedElement}`;
+    return config;
+  }
+}
+
+const getElementsToCollapse = (bpmnDiagramName: string): Array<string> => {
+  const elementsToCollapse = elementsToCollapsePerDiagram.get(bpmnDiagramName);
+  // add 'none' to test the diagram rendering without collapsing any element. There is no element in diagrams with the 'none' id (in this case, the value is ignored  and there is no collapsing).
+  return ['none', ...elementsToCollapse];
+};
+
+describe('Collapse BPMN elements', () => {
+  const diagramSubfolder = 'collapse-expand';
+  const imageSnapshotConfigurator = new CollapsedElementImageSnapshotConfigurator(
+    new MultiBrowserImageSnapshotThresholds({ chromium: 0, firefox: 0 / 100, webkit: 0 / 100 }),
+    diagramSubfolder,
+  );
+  const pageTester = new PageTester({ pageFileName: 'non-regression', expectedPageTitle: 'BPMN Visualization Non Regression', diagramSubfolder }, <Page>page);
+  const bpmnDiagramNames = getBpmnDiagramNames(diagramSubfolder);
+
+  describe.each(bpmnDiagramNames)(`%s`, (bpmnDiagramName: string) => {
+    it.each(getElementsToCollapse(bpmnDiagramName))(`collapse %s`, async (bpmnElementIdToCollapse: string) => {
+      await pageTester.gotoPageAndLoadBpmnDiagram(bpmnDiagramName, {
+        bpmnElementIdToCollapse: bpmnElementIdToCollapse,
+      });
+      const image = await page.screenshot({ fullPage: true });
+      const config = imageSnapshotConfigurator.getConfig({ fileName: bpmnDiagramName, collapsedElement: bpmnElementIdToCollapse });
+      expect(image).toMatchImageSnapshot(config);
+    });
+  });
+});
