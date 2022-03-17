ac8125467c8df8af126f005cdcb203527e9a9964
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-03-17 14:49:49 +0100

    bpmn.elements.collapsed.test.ts threshold firefox all OS

--- test/e2e/bpmn.elements.collapsed.test.ts
+++ test/e2e/bpmn.elements.collapsed.test.ts
@@ -44,10 +44,9 @@ const getElementsToCollapse = (bpmnDiagramName: string): Array<string> => {
 describe('Collapse BPMN elements', () => {
   const diagramSubfolder = 'collapse-expand';
   const imageSnapshotConfigurator = new CollapsedElementImageSnapshotConfigurator(
-    // firefox: max 0.2640822887892802%
+    // firefox: max 0.10839637777485533%
     // webkit: max 0.14363687914162873%
-    // new MultiBrowserImageSnapshotThresholds({ chromium: 0, firefox: 0.27 / 100, webkit: 0.3 / 100 }),
-    new MultiBrowserImageSnapshotThresholds({ chromium: 0, firefox: 0 / 100, webkit: 0.15 / 100 }),
+    new MultiBrowserImageSnapshotThresholds({ chromium: 0, firefox: 0.11 / 100, webkit: 0.15 / 100 }),
     diagramSubfolder,
   );
   const pageTester = new PageTester({ pageFileName: 'non-regression', expectedPageTitle: 'BPMN Visualization Non Regression', diagramSubfolder }, <Page>page);
