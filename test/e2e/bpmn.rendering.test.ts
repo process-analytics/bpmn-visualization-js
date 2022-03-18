ba9107425669de5f884ef5f2608cf10f5eac1c3e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-03-18 22:53:09 +0100

    bpmn.rendering.test.ts: update screenshots + tmp disable thresholds for reconfiguration
    recreate screenshots that involve labels

--- test/e2e/bpmn.rendering.test.ts
+++ test/e2e/bpmn.rendering.test.ts
@@ -34,75 +34,75 @@ class ImageSnapshotThresholds extends MultiBrowserImageSnapshotThresholds {
       [
         'flows.message.02.labels.and.complex.paths',
         {
-          macos: 0.05 / 100, // 0.042940114894829406%
-          windows: 0.13 / 100, // 0.12414788622622241%
+          // macos: 0.05 / 100, // 0.042940114894829406%
+          // windows: 0.13 / 100, // 0.12414788622622241%
         },
       ],
       [
         'group.01.in.process.with.label',
         {
-          macos: 0.0002, // 0.017014812909055266%
-          windows: 0.0003, // 0.026260389475118995%
+          // macos: 0.0002, // 0.017014812909055266%
+          // windows: 0.0003, // 0.026260389475118995%
         },
       ],
       [
         'group.02.in.collaboration.with.label',
         {
-          macos: 0.0002, // 0.011733173038031008%
-          windows: 0.0004, // 0.030760508478666626%
+          // macos: 0.0002, // 0.011733173038031008%
+          // windows: 0.0004, // 0.030760508478666626%
         },
       ],
       [
         'labels.01.general',
         {
-          linux: 0.0005 / 100, // 0.0004690507665716659%
-          macos: 0.63 / 100, // 0.6225356945699145%
-          windows: 0.48 / 100, // 0.4794467924774448%
+          // linux: 0.0005 / 100, // 0.0004690507665716659%
+          // macos: 0.63 / 100, // 0.6225356945699145%
+          // windows: 0.48 / 100, // 0.4794467924774448%
         },
       ],
       [
         'labels.02.position.and.line.breaks',
         {
-          linux: 0.0089, // 0.8846259528850542%
-          windows: 0.082, // 0.8197899179969159%
+          // linux: 0.0089, // 0.8846259528850542%
+          // windows: 0.082, // 0.8197899179969159%
         },
       ],
       [
         'labels.03.default.position',
         {
-          linux: 0.0009 / 100, // 0.0008448953993434039%
-          macos: 0.47 / 100, // 0.46761316449689394%
-          windows: 0.32 / 100, // 0.31624569609181025%
+          // linux: 0.0009 / 100, // 0.0008448953993434039%
+          // macos: 0.47 / 100, // 0.46761316449689394%
+          // windows: 0.32 / 100, // 0.31624569609181025%
         },
       ],
       [
         'labels.04.fonts',
         {
-          macos: 0.19 / 100, // 0.18334725431882193%
-          windows: 0.22 / 100, // 0.21293572507528058%
+          // macos: 0.19 / 100, // 0.18334725431882193%
+          // windows: 0.22 / 100, // 0.21293572507528058%
         },
       ],
       [
         'pools.01.labels.and.lanes',
         {
-          macos: 0.13 / 100, // 0.1232401834606045%
-          windows: 0.25 / 100, // 0.20513086293463267%
+          // macos: 0.13 / 100, // 0.1232401834606045%
+          // windows: 0.25 / 100, // 0.20513086293463267%
         },
       ],
       [
         'pools.02.vertical.with.lanes',
         {
-          linux: 0.0014, // 0.13132100299135807%
-          macos: 0.0015, // 0.14776609441433664%
-          windows: 0.002, // 0.1182792778311903%
+          // linux: 0.0014, // 0.13132100299135807%
+          // macos: 0.0015, // 0.14776609441433664%
+          // windows: 0.002, // 0.1182792778311903%
         },
       ],
       [
         'pools.03.black.box',
         {
-          linux: 0.0017, // 0.16083207782319198%
-          macos: 0.0018, // 0.17300897532621654%
-          windows: 0.0026, // 0.25283466417285183%
+          // linux: 0.0017, // 0.16083207782319198%
+          // macos: 0.0018, // 0.17300897532621654%
+          // windows: 0.0026, // 0.25283466417285183%
         },
       ],
       // tests without labels
@@ -132,84 +132,84 @@ class ImageSnapshotThresholds extends MultiBrowserImageSnapshotThresholds {
       [
         'flows.message.02.labels.and.complex.paths',
         {
-          linux: 0.09 / 100, // 0.0833293935339152%
-          macos: 0.13 / 100, // 0.12587692325856104%
-          windows: 0.71 / 100, // 0.7024096538645774%
+          // linux: 0.09 / 100, // 0.0833293935339152%
+          // macos: 0.13 / 100, // 0.12587692325856104%
+          // windows: 0.71 / 100, // 0.7024096538645774%
         },
       ],
       [
         'group.01.in.process.with.label',
         {
-          macos: 0.0002, // 0.017539672871980105%
-          windows: 0.0022, // 0.21161062086058058%
+          // macos: 0.0002, // 0.017539672871980105%
+          // windows: 0.0022, // 0.21161062086058058%
         },
       ],
       [
         'group.02.in.collaboration.with.label',
         {
-          macos: 0.0002, // 0.015546117401621373%
-          windows: 0.0019, // 0.18223382519321207%
+          // macos: 0.0002, // 0.015546117401621373%
+          // windows: 0.0019, // 0.18223382519321207%
         },
       ],
       [
         'labels.01.general',
         {
           // high values due to font rendering discrepancies with chromium rendering
-          linux: 2 / 100, // 1.99314755236637%
-          macos: 2.35 / 100, // 2.342807360599164%
-          // very high threshold
-          windows: 13.53, // 13.521318196938303%
+          // linux: 2 / 100, // 1.99314755236637%
+          // macos: 2.35 / 100, // 2.342807360599164%
+          // // very high threshold
+          // windows: 13.53, // 13.521318196938303%
         },
       ],
       [
         'labels.02.position.and.line.breaks',
         {
           // TODO possible rendering issue so high threshold value
-          linux: 0.031, // 3.08975009972805%
-          macos: 0.025, // 2.478877395928847%
-          // very high threshold
-          windows: 0.156, // 15.594367802739583%
+          // linux: 0.031, // 3.08975009972805%
+          // macos: 0.025, // 2.478877395928847%
+          // // very high threshold
+          // windows: 0.156, // 15.594367802739583%
         },
       ],
       [
         'labels.03.default.position',
         {
-          linux: 0.41 / 100, // 0.40701080345780793%
-          macos: 0.82 / 100, // 0.8111318088812269%
+          // linux: 0.41 / 100, // 0.40701080345780793%
+          // macos: 0.82 / 100, // 0.8111318088812269%
           // // TODO possible rendering issue so high threshold value
-          windows: 2.73, // 2.722838502156999%
+          // windows: 2.73, // 2.722838502156999%
         },
       ],
       [
         'labels.04.fonts',
         {
           // high values due to font rendering discrepancies with chromium rendering
-          linux: 1.46 / 100, // 1.4556545630196482%
-          macos: 0.91 / 100, // 0.904039586436256%
-          windows: 1.73, // 1.7261364524240324%
+          // linux: 1.46 / 100, // 1.4556545630196482%
+          // macos: 0.91 / 100, // 0.904039586436256%
+          // windows: 1.73, // 1.7261364524240324%
         },
       ],
       [
         'pools.01.labels.and.lanes',
         {
-          macos: 0.13 / 100, // 0.1272045431662927%
-          windows: 0.59 / 100, // 0.5809316243215457%
+          // macos: 0.13 / 100, // 0.1272045431662927%
+          // windows: 0.59 / 100, // 0.5809316243215457%
         },
       ],
       [
         'pools.02.vertical.with.lanes',
         {
-          linux: 0.0003, // 0.024310386980885834%
-          macos: 0.0015, // 0.14753246729929392%
-          windows: 0.0072, // 0.7100461008680559%
+          // linux: 0.0003, // 0.024310386980885834%
+          // macos: 0.0015, // 0.14753246729929392%
+          // windows: 0.0072, // 0.7100461008680559%
         },
       ],
       [
         'pools.03.black.box',
         {
-          linux: 0.0023, // 0.22636282955619252%
-          macos: 0.0022, // 0.21007470164219333%
-          windows: 0.0059, // 0.5856398049701173%
+          // linux: 0.0023, // 0.22636282955619252%
+          // macos: 0.0022, // 0.21007470164219333%
+          // windows: 0.0059, // 0.5856398049701173%
         },
       ],
       // tests without labels
@@ -245,51 +245,51 @@ class ImageSnapshotThresholds extends MultiBrowserImageSnapshotThresholds {
       [
         'flows.message.02.labels.and.complex.paths',
         {
-          macos: 0.41 / 100, // 0.40016434760490327%
+          // macos: 0.41 / 100, // 0.40016434760490327%
         },
       ],
       [
         'labels.01.general',
         {
           // TODO possible rendering issue so high threshold value
-          macos: 2.61 / 100, // 2.606652827219813%
+          // macos: 2.61 / 100, // 2.606652827219813%
         },
       ],
       [
         'labels.02.position.and.line.breaks',
         {
           // TODO possible rendering issue so high threshold value
-          macos: 0.059, // 5.875189976297179%
+          // macos: 0.059, // 5.875189976297179%
         },
       ],
       [
         'labels.03.default.position',
         {
-          macos: 0.85 / 100, // 0.843063100064223%
+          // macos: 0.85 / 100, // 0.843063100064223%
         },
       ],
       [
         'labels.04.fonts',
         {
-          macos: 0.63 / 100, // 0.6255817109336315%
+          // macos: 0.63 / 100, // 0.6255817109336315%
         },
       ],
       [
         'pools.01.labels.and.lanes',
         {
-          macos: 0.25 / 100, // 0.2428126684001386%
+          // macos: 0.25 / 100, // 0.2428126684001386%
         },
       ],
       [
         'pools.02.vertical.with.lanes',
         {
-          macos: 0.0026, // 0.24229404068003557%
+          // macos: 0.0026, // 0.24229404068003557%
         },
       ],
       [
         'pools.03.black.box',
         {
-          macos: 0.0023, // 0.2242640286478026%
+          // macos: 0.0023, // 0.2242640286478026%
         },
       ],
       // tests without labels
