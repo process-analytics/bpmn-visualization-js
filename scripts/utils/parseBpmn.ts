8cb96402bf4f143c204a196049637e344963211a
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-04-27 16:54:28 +0200

    improve logs of the parseBpmn utils

--- scripts/utils/parseBpmn.ts
+++ scripts/utils/parseBpmn.ts
@@ -28,12 +28,16 @@ const argv = parseArgs(process.argv.slice(2));
 const bpmnFilePath = argv._[0];
 const outputType = argv['output'] || 'json';
 
+// eslint-disable-next-line no-console
+console.info('Generating BPMN in the "%s" output type', outputType);
 if (!bpmnFilePath) {
-  throw new Error('you must provide file path as 1st parameter for example: test/fixtures/bpmn/parser-test.bpmn');
+  throw new Error('You must provide file path as 1st parameter for example: test/fixtures/bpmn/simple-start-task-end.bpmn ');
 }
 if (['json', 'model'].indexOf(outputType) == -1) {
   throw new Error('--output parameter must be one of: json | model');
 }
+// eslint-disable-next-line no-console
+console.info('Use BPMN diagram located at "%s"', bpmnFilePath);
 
 const xmlParser = new BpmnXmlParser();
 const json = xmlParser.parse(readFileSync(bpmnFilePath, 'utf-8', __dirname));
@@ -55,6 +59,10 @@ if (outputType === 'json') {
 const isRunningOnCIWithLinuxOS = process.env.CI === 'true' && process.platform.startsWith('linux');
 if (!isRunningOnCIWithLinuxOS) {
   clipboardy.writeSync(result);
+} else {
+  console.warn('No clipboard copy, as it is not supported in this environment');
 }
-// eslint-disable-next-line no-console
-console.log(result);
+
+/* eslint-disable no-console */
+console.info('Output generated');
+console.info(result);
