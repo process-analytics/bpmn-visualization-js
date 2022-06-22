22436954a721ae7beaf8598674b4b111efec7f82
Souchet Céline <celine.souchet@bonitasoft.com>
2022-06-22 18:32:19 +0200

    Fix lint

--- src/component/registry/bpmn-model-filters.ts
+++ src/component/registry/bpmn-model-filters.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import type { Edge } from '../../model/bpmn/internal/edge/edge';
 import type BpmnModel from '../../model/bpmn/internal/BpmnModel';
 import type { ModelFilter } from '../options';
 import { ensureIsArray } from '../helpers/array-utils';
