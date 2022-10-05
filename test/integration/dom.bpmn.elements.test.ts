53a42bdf67527210519a809ec6c48b96506359d9
Souchet Céline <celine.souchet@bonitasoft.com>
2022-10-05 13:44:31 +0200

    Add tests on BPMN registry

--- test/integration/dom.bpmn.elements.test.ts
+++ test/integration/dom.bpmn.elements.test.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import type { BpmnVisualization } from '../../src/bpmn-visualization';
 import { FlowKind, ShapeBpmnElementKind } from '../../src/bpmn-visualization';
 import {
   expectEndEventBpmnElement,
@@ -22,77 +23,90 @@ import {
   expectStartEventBpmnElement,
   expectTaskBpmnElement,
 } from './helpers/semantic-with-svg-utils';
-import { initializeBpmnVisualizationWithContainerId } from './helpers/bpmn-visualization-initialization';
+import {
+  initializeBpmnVisualization,
+  initializeBpmnVisualizationWithContainerId,
+  initializeBpmnVisualizationWithHtmlElement,
+  initializeBpmnVisualizationWithoutId,
+} from './helpers/bpmn-visualization-initialization';
 import { readFileSync } from '../helpers/file-helper';
 
 describe('Bpmn Elements registry - retrieve BPMN elements', () => {
-  const bpmnVisualization = initializeBpmnVisualizationWithContainerId();
+  describe.each`
+    bpmnVisualization                               | type
+    ${initializeBpmnVisualizationWithContainerId()} | ${'html id'}
+    ${initializeBpmnVisualizationWithHtmlElement()} | ${'html element'}
+    ${initializeBpmnVisualizationWithoutId()}       | ${'html element without id'}
+    ${initializeBpmnVisualization('')}              | ${'html element with id set to empty string'}
+    ${initializeBpmnVisualization(null)}            | ${'html element with id set to null'}
+    ${initializeBpmnVisualization(undefined)}       | ${'html element with id set to undefined'}
+  `('container set with "$type"', ({ bpmnVisualization }: { bpmnVisualization: BpmnVisualization }) => {
+    describe('Get by ids', () => {
+      it('Pass several existing ids', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
 
-  describe('Get by ids', () => {
-    it('Pass several existing ids', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds(['StartEvent_1', 'Flow_2']);
+        expect(bpmnElements).toHaveLength(2);
 
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds(['StartEvent_1', 'Flow_2']);
-      expect(bpmnElements).toHaveLength(2);
+        expectStartEventBpmnElement(bpmnElements[0], { id: 'StartEvent_1', name: 'Start Event 1' });
+        expectSequenceFlowBpmnElement(bpmnElements[1], { id: 'Flow_2' });
+      });
 
-      expectStartEventBpmnElement(bpmnElements[0], { id: 'StartEvent_1', name: 'Start Event 1' });
-      expectSequenceFlowBpmnElement(bpmnElements[1], { id: 'Flow_2' });
+      it('Pass a single non existing id', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds('unknown');
+        expect(bpmnElements).toHaveLength(0);
+      });
+
+      it('Pass existing and non existing ids', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds(['StartEvent_1', 'unknown', 'Flow_1', 'another_unknown']);
+        expect(bpmnElements).toHaveLength(2);
+      });
     });
 
-    it('Pass a single non existing id', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds('unknown');
-      expect(bpmnElements).toHaveLength(0);
-    });
+    describe('Get by kinds', () => {
+      it('Pass a single kind related to a single existing element', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(ShapeBpmnElementKind.TASK);
+        expect(bpmnElements).toHaveLength(1);
 
-    it('Pass existing and non existing ids', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByIds(['StartEvent_1', 'unknown', 'Flow_1', 'another_unknown']);
-      expect(bpmnElements).toHaveLength(2);
-    });
-  });
+        expectTaskBpmnElement(bpmnElements[0], { id: 'Activity_1', name: 'Task 1' });
+      });
 
-  describe('Get by kinds', () => {
-    it('Pass a single kind related to a single existing element', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(ShapeBpmnElementKind.TASK);
-      expect(bpmnElements).toHaveLength(1);
+      it('Pass a single kind related to several existing elements', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(FlowKind.SEQUENCE_FLOW);
+        expect(bpmnElements).toHaveLength(2);
 
-      expectTaskBpmnElement(bpmnElements[0], { id: 'Activity_1', name: 'Task 1' });
-    });
+        expectSequenceFlowBpmnElement(bpmnElements[0], { id: 'Flow_1', name: 'Sequence Flow 1' });
+        expectSequenceFlowBpmnElement(bpmnElements[1], { id: 'Flow_2' });
+      });
 
-    it('Pass a single kind related to several existing elements', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(FlowKind.SEQUENCE_FLOW);
-      expect(bpmnElements).toHaveLength(2);
+      it('No elements for this kind', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(ShapeBpmnElementKind.SUB_PROCESS);
+        expect(bpmnElements).toHaveLength(0);
+      });
 
-      expectSequenceFlowBpmnElement(bpmnElements[0], { id: 'Flow_1', name: 'Sequence Flow 1' });
-      expectSequenceFlowBpmnElement(bpmnElements[1], { id: 'Flow_2' });
-    });
+      it('Pass a several kinds that all match existing elements', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/registry/1-pool-3-lanes-message-start-end-intermediate-events.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds([ShapeBpmnElementKind.EVENT_END, ShapeBpmnElementKind.POOL]);
+        expect(bpmnElements).toHaveLength(3);
 
-    it('No elements for this kind', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/simple-start-task-end.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds(ShapeBpmnElementKind.SUB_PROCESS);
-      expect(bpmnElements).toHaveLength(0);
-    });
+        expectEndEventBpmnElement(bpmnElements[0], { id: 'endEvent_terminate_1', name: 'terminate end 1' });
+        expectEndEventBpmnElement(bpmnElements[1], { id: 'endEvent_message_1', name: 'message end 2' });
+        expectPoolBpmnElement(bpmnElements[2], { id: 'Participant_1', name: 'Pool 1' });
+      });
 
-    it('Pass a several kinds that all match existing elements', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/registry/1-pool-3-lanes-message-start-end-intermediate-events.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds([ShapeBpmnElementKind.EVENT_END, ShapeBpmnElementKind.POOL]);
-      expect(bpmnElements).toHaveLength(3);
+      it('Pass a several kinds that match existing and non-existing elements', async () => {
+        bpmnVisualization.load(readFileSync('../fixtures/bpmn/registry/1-pool-3-lanes-message-start-end-intermediate-events.bpmn'));
+        const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds([ShapeBpmnElementKind.CALL_ACTIVITY, ShapeBpmnElementKind.TASK_SERVICE]);
+        expect(bpmnElements).toHaveLength(2);
 
-      expectEndEventBpmnElement(bpmnElements[0], { id: 'endEvent_terminate_1', name: 'terminate end 1' });
-      expectEndEventBpmnElement(bpmnElements[1], { id: 'endEvent_message_1', name: 'message end 2' });
-      expectPoolBpmnElement(bpmnElements[2], { id: 'Participant_1', name: 'Pool 1' });
-    });
-
-    it('Pass a several kinds that match existing and non-existing elements', async () => {
-      bpmnVisualization.load(readFileSync('../fixtures/bpmn/registry/1-pool-3-lanes-message-start-end-intermediate-events.bpmn'));
-      const bpmnElements = bpmnVisualization.bpmnElementsRegistry.getElementsByKinds([ShapeBpmnElementKind.CALL_ACTIVITY, ShapeBpmnElementKind.TASK_SERVICE]);
-      expect(bpmnElements).toHaveLength(2);
-
-      expectServiceTaskBpmnElement(bpmnElements[0], { id: 'serviceTask_1_2', name: 'Service Task 1.2' });
-      expectServiceTaskBpmnElement(bpmnElements[1], { id: 'serviceTask_2_1', name: 'Service Task 2.1' });
+        expectServiceTaskBpmnElement(bpmnElements[0], { id: 'serviceTask_1_2', name: 'Service Task 1.2' });
+        expectServiceTaskBpmnElement(bpmnElements[1], { id: 'serviceTask_2_1', name: 'Service Task 2.1' });
+      });
     });
   });
 });
