e34d0cfbdf3a101dcd3e64e41e80b10284c7dea7
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2022-10-14 07:45:23 +0200

    JsonTestUtils.ts: fix name of a constant

--- test/unit/helpers/JsonTestUtils.ts
+++ test/unit/helpers/JsonTestUtils.ts
@@ -115,11 +115,11 @@ export function parseJsonAndExpectOnlyEdgesAndFlowNodes(json: BpmnJsonModel, num
 }
 
 export function verifySubProcess(model: BpmnModel, kind: ShapeBpmnSubProcessKind, expectedNumber: number): void {
-  const events = model.flowNodes.filter(shape => {
+  const subProcesses = model.flowNodes.filter(shape => {
     const bpmnElement = shape.bpmnElement;
     return bpmnElement instanceof ShapeBpmnSubProcess && (bpmnElement as ShapeBpmnSubProcess).subProcessKind === kind;
   });
-  expect(events).toHaveLength(expectedNumber);
+  expect(subProcesses).toHaveLength(expectedNumber);
 }
 
 export function verifyLabelFont(label: Label, expectedFont?: ExpectedFont): void {
