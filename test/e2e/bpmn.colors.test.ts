44563ff3c8dbf4d78d5df0d8c2e4b3b29d213179
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-04-09 21:34:14 +0200

    e2e test: remove unused import

--- test/e2e/bpmn.colors.test.ts
+++ test/e2e/bpmn.colors.test.ts
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import type { StyleOptions } from './helpers/visu/bpmn-page-utils';
 import { AvailableTestPages, PageTester } from './helpers/visu/bpmn-page-utils';
 import { ImageSnapshotConfigurator, MultiBrowserImageSnapshotThresholds } from './helpers/visu/image-snapshot-config';
 import type { Page } from 'playwright';
