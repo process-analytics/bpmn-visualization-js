94db78b36087c428d8cb6dab89e19e5c1efc3d84
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-04-12 12:06:29 +0200

    e2e: configure thresholds

--- test/e2e/bpmn.colors.test.ts
+++ test/e2e/bpmn.colors.test.ts
@@ -15,12 +15,57 @@ limitations under the License.
 */
 
 import { AvailableTestPages, PageTester } from './helpers/visu/bpmn-page-utils';
+import type { ImageSnapshotThresholdConfig } from './helpers/visu/image-snapshot-config';
 import { ImageSnapshotConfigurator, MultiBrowserImageSnapshotThresholds } from './helpers/visu/image-snapshot-config';
 import type { Page } from 'playwright';
 import { getBpmnDiagramNames } from './helpers/test-utils';
 
+class ImageSnapshotThresholds extends MultiBrowserImageSnapshotThresholds {
+  constructor() {
+    // threshold for webkit is taken from macOS only
+    super({ chromium: 0 / 100, firefox: 0.006 / 100, webkit: 0.07 / 100 });
+  }
+
+  protected override getChromiumThresholds(): Map<string, ImageSnapshotThresholdConfig> {
+    return new Map<string, ImageSnapshotThresholdConfig>([
+      [
+        'elements.colors.02.labels',
+        {
+          linux: 0.0004 / 100, // 0.0003629132815818892%
+          macos: 0.15 / 100, // 0.14367268757742302%
+          windows: 0.1 / 100, // 0.09608375248252311%
+        },
+      ],
+    ]);
+  }
+
+  protected override getFirefoxThresholds(): Map<string, ImageSnapshotThresholdConfig> {
+    return new Map<string, ImageSnapshotThresholdConfig>([
+      [
+        'elements.colors.02.labels',
+        {
+          linux: 0.06 / 100, // 0.053125174438761746%
+          macos: 0 / 100, // 0.%
+          windows: 1.76 / 100, // 1.7546495847922894%
+        },
+      ],
+    ]);
+  }
+
+  protected override getWebkitThresholds(): Map<string, ImageSnapshotThresholdConfig> {
+    return new Map<string, ImageSnapshotThresholdConfig>([
+      [
+        'elements.colors.02.labels',
+        {
+          macos: 0.41 / 100, // 0.40831372531949794%
+        },
+      ],
+    ]);
+  }
+}
+
 describe('BPMN in color', () => {
-  const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new MultiBrowserImageSnapshotThresholds({ chromium: 0 / 100, firefox: 0 / 100, webkit: 0 / 100 }), 'bpmn-colors');
+  const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new ImageSnapshotThresholds(), 'bpmn-colors');
 
   const diagramSubfolder = 'bpmn-in-color';
   const pageTester = new PageTester({ targetedPage: AvailableTestPages.BPMN_RENDERING, diagramSubfolder }, <Page>page);
