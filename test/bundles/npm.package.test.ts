a1619667c26ea0e40bb8a9930192cca3c27ce0e6
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-05-02 16:33:31 +0200

    simplify some imports

--- test/bundles/npm.package.test.ts
+++ test/bundles/npm.package.test.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { findFiles } from '../shared/file-helper';
+import { findFiles } from '@test/shared/file-helper';
 
 describe('NPM package', () => {
   describe('Files in the dist directory', () => {
