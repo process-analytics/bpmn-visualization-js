0610b333b8b657032944e75a4dd9e82a24bf3e30
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-06-04 23:49:52 +0200

    Revert comment update in icon-painter.ts
    We only want to see the technical impacts of the migration to maxGraph here
    Comment/variables will be updated while or after doing the actual migration

--- src/component/mxgraph/shape/render/icon-painter.ts
+++ src/component/mxgraph/shape/render/icon-painter.ts
@@ -90,7 +90,7 @@ export class IconPainter {
   /**
    * Utility paint icon methods to easily instantiate a {@link BpmnCanvas} from a {@link PaintParameter}.
    *
-   * @param canvas                  mxgraph `AbstractCanvas2D` in charge of performing the paint operations.
+   * @param canvas                  mxgraph `mxAbstractCanvas2D` in charge of performing the paint operations.
    * @param ratioFromParent         the actual size of the icon will be computed from the shape dimensions using this ratio.
    * @param setIconOriginFunct      called function to set the origin of the icon. Generally, it calls a method of {@link BpmnCanvas}.
    * @param shapeConfig             dimension and style of the shape where the icon is painted.
