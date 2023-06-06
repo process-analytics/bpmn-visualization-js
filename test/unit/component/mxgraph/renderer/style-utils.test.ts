c674eecf537b7192c0b84e7b0b0213cd72880a2f
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-06-06 11:00:57 +0200

    style-utils.test.ts: restore test removed by mistake (sub-process transaction)

--- test/unit/component/mxgraph/renderer/style-utils.test.ts
+++ test/unit/component/mxgraph/renderer/style-utils.test.ts
@@ -60,9 +60,10 @@ describe('compute all css class names based on style input', () => {
     ${ShapeBpmnElementKind.TASK}                                                         | ${true}  | ${['bpmn-type-activity', 'bpmn-type-task', 'bpmn-task', 'bpmn-label']}
     ${ShapeBpmnElementKind.TASK_BUSINESS_RULE}                                           | ${false} | ${['bpmn-type-activity', 'bpmn-type-task', 'bpmn-business-rule-task']}
     ${ShapeBpmnElementKind.SUB_PROCESS}                                                  | ${false} | ${['bpmn-type-activity', 'bpmn-sub-process']}
+    ${'subProcess;bpmn.subProcessKind=adHoc'}                                            | ${true}  | ${['bpmn-type-activity', 'bpmn-sub-process', 'bpmn-sub-process-adhoc', 'bpmn-label']}
     ${'subProcess;bpmn.subProcessKind=embedded'}                                         | ${false} | ${['bpmn-type-activity', 'bpmn-sub-process', 'bpmn-sub-process-embedded']}
     ${'subProcess;bpmn.subProcessKind=event'}                                            | ${true}  | ${['bpmn-type-activity', 'bpmn-sub-process', 'bpmn-sub-process-event', 'bpmn-label']}
-    ${'subProcess;bpmn.subProcessKind=adHoc'}                                            | ${true}  | ${['bpmn-type-activity', 'bpmn-sub-process', 'bpmn-sub-process-adhoc', 'bpmn-label']}
+    ${'subProcess;bpmn.subProcessKind=transaction'}                                      | ${true}  | ${['bpmn-type-activity', 'bpmn-sub-process', 'bpmn-sub-process-transaction', 'bpmn-label']}
     ${FlowKind.ASSOCIATION_FLOW}                                                         | ${true}  | ${['bpmn-type-flow', 'bpmn-association', 'bpmn-label']}
     ${FlowKind.MESSAGE_FLOW}                                                             | ${false} | ${['bpmn-type-flow', 'bpmn-message-flow']}
     ${'sequenceFlow;default;fontStyle=4'}                                                | ${false} | ${['bpmn-type-flow', 'bpmn-sequence-flow']}
