3ee7b24fb3c89010680aad4af61a67237b3db51f
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-08-17 15:04:13 +0200

    move main.ts

--- dev/ts/dev-bundle-index.ts
+++ dev/ts/dev-bundle-index.ts
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-export * from './main';
+export * from './utils/main';
 export * from './utils/shared-helpers';
 export * from '../../src/bpmn-visualization';
