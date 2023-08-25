a3d2e44b7e3803bf19535780d5269bb19cecfa66
Souchet Céline <celine.souchet@bonitasoft.com>
2023-08-25 15:10:39 +0200

    Replace 'plugin:@typescript-eslint/stylistic' by 'plugin:@typescript-eslint/stylistic-type-checked' rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -94,7 +94,7 @@ module.exports = {
       extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        'plugin:@typescript-eslint/stylistic',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:import/typescript',
       ],
       settings: {
