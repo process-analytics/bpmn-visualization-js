5cc78b7b6498d44dcaaaafdfb8b4ee6f26c9bfed
Souchet Céline <celine.souchet@bonitasoft.com>
2023-08-25 15:00:07 +0200

    Add 'plugin:@typescript-eslint/stylistic' rule
    (cherry picked from commit 63e073e325da41b2a16ddcb0fa5b6865af994fb0)

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
       extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'plugin:@typescript-eslint/stylistic',
       ],
       parserOptions: {
         // This setting is required if you want to use rules which require type information
