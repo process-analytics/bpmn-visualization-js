0dc498064766ee6534bb50048a69735f3ed2da28
Souchet Céline <celine.souchet@bonitasoft.com>
2023-08-29 10:47:00 +0200

    enable '@typescript-eslint/no-misused-promises' rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -80,6 +80,7 @@ module.exports = {
         
         '@typescript-eslint/require-await': 'error',
         '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
       },
     },
   ],
