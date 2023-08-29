f9a1baf44e56fb6f587388a6b89fcdd4888ca7ef
Souchet Céline <celine.souchet@bonitasoft.com>
2023-08-29 10:38:35 +0200

    enable  '@typescript-eslint/require-await' rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -77,6 +77,8 @@ module.exports = {
         '@typescript-eslint/consistent-type-imports': ['error'],
         // We choose to disable it and choose later if we want to enable it. See https://github.com/process-analytics/bpmn-visualization-js/pull/2821.
         '@typescript-eslint/consistent-type-definitions': 'off',
+        
+        '@typescript-eslint/require-await': 'error',
       },
     },
   ],
