49e9d8ab8e84cbb4c370db4a18f405ba87719bd6
Souchet Céline <celine.souchet@bonitasoft.com>
2023-09-01 10:52:24 +0200

    add plugin:import/recommended rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,7 @@ module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:import/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
