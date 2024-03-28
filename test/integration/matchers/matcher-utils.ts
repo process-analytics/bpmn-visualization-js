1de1909e6272c0632e01bb40bd0ad37bd999949c
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-03-28 14:55:40 +0100

    integ test: restore the implem of the getFontStyleValue utils function (make 2 tests pass)

--- test/integration/matchers/matcher-utils.ts
+++ test/integration/matchers/matcher-utils.ts
@@ -130,11 +130,12 @@ export function buildCellMatcher<R>(
 }
 
 export function getFontStyleValue(expectedFont: ExpectedFont): number {
-  return expectedFont
-    ? computeFontStyleValue(new Font(expectedFont.family, expectedFont.size, expectedFont.isBold, expectedFont.isItalic, expectedFont.isUnderline, expectedFont.isStrikeThrough))
-    : // TODO maxgraph@0.1.0 decide if we set the fontStyle property to 0 or if we omit it
-      // : 0) || undefined
-      undefined;
+  // TODO maxgraph@0.1.0 decide if we set the fontStyle property to 0 or if we omit it
+  return (
+    (expectedFont
+      ? computeFontStyleValue(new Font(expectedFont.family, expectedFont.size, expectedFont.isBold, expectedFont.isItalic, expectedFont.isUnderline, expectedFont.isStrikeThrough))
+      : 0) || undefined
+  );
 }
 
 export function buildExpectedCellStyleWithCommonAttributes(expectedModelElt: ExpectedEdgeModelElement | ExpectedShapeModelElement): BpmnCellStyle {
