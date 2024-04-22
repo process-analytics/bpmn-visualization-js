bc37276d32f89ae695e51dd31a45ae1e36ee558e
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-04-22 18:21:36 +0200

    EXTRA WIP run integration in ESM mode

---
+++ test/integration/jest.config.mjs
@@ -0,0 +1,96 @@
+/*
+Copyright 2020 Bonitasoft S.A.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { moduleNameMapper } from '../config/ts-jest-module-mapper.mjs';
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  rootDir: '../..',
+  roots: ['./test/integration'],
+  testMatch: ['**/?(*.)+(spec|test).[t]s'],
+  testEnvironment: 'jsdom', // mxgraph initialization needs to access to the window object
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: '<rootDir>/tsconfig.test.json',
+      },
+    ],
+  },
+  // moduleNameMapper: {
+  //   ...moduleNameMapper,
+  //   // Hack to use lodash instead of lodash-es in integration tests.
+  //   // This is only to resolve the import, otherwise Jest fails to parse the lodash-es files.
+  //   // For more details, see https://github.com/process-analytics/bpmn-visualization-js/pull/2678
+  //   // The lodash code is not called in integration tests, so changing the lodash implementation in used in not an issue.
+  //   '^lodash-es$': 'lodash',
+  // },
+  moduleNameMapper,
+  // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleFileExtensions: ['ts', 'js', 'mjs', 'cjs', 'jsx', 'tsx', 'json', 'node'],
+  collectCoverageFrom: ['src/**/*.{ts,js}'],
+  coveragePathIgnorePatterns: ['/src/model/'],
+  coverageReporters: ['lcov', 'text-summary'],
+  coverageDirectory: 'build/test-report/integration',
+  // setupFilesAfterEnv: [
+  //   './test/integration/config/mxgraph-config.ts',
+  //   // put at the latest place to see logs that could be displayed by setup files
+  //   './test/integration/config/hide-console-warnings.js',
+  // ],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        pageTitle: 'bpmn-visualization Integration Test Report',
+        outputPath: 'build/test-report/integration/index.html',
+        includeFailureMsg: true,
+        includeSuiteFailure: true,
+      },
+    ],
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'build/test-report/integration',
+      },
+    ],
+  ],
+};
+
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// const config2 = {
+//   collectCoverageFrom: ['src/**/*.{ts,js}'],
+//   coverageReporters: ['html', 'text-summary'],
+//   extensionsToTreatAsEsm: ['.ts'],
+//   // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+//   moduleFileExtensions: ['ts', 'js', 'mjs', 'cjs', 'jsx', 'tsx', 'json', 'node'],
+//   setupFilesAfterEnv: ['<rootDir>/config/jest-setup.js'],
+//   testEnvironment: 'jsdom', // let access to the browser objects
+//   testMatch: ['**/test/**/?(*.)+(spec|test).[tj]s?(x)'],
+//   transform: {
+//     '^.+\\.ts?$': [
+//       'ts-jest',
+//       {
+//         useESM: true,
+//         tsconfig: '<rootDir>/tsconfig.test.json',
+//       },
+//     ],
+//   },
+// };
+
+export default config;
