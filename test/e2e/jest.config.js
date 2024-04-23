c9259664d7a8e245302c1594da9eb7fa8c692561
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-04-23 14:16:35 +0200

    WIP e2e tests run in ESM - tmp jest.image configuration

--- test/e2e/jest.config.js
+++ test/e2e/jest.config.js
@@ -48,7 +48,8 @@ module.exports = {
     'expect-playwright',
     './test/config/jest.retries.ts',
     // jest-image-snapshot configuration doesn't work with setupFiles, fix with setupFilesAfterEnv: see https://github.com/testing-library/jest-dom/issues/122#issuecomment-650520461
-    './test/config/jest.image.ts',
+    './test/config/jest.image.tmp.ts',
+    // './test/config/jest.image.ts',
     // need playwright globals to be available, so after environment
     './test/config/playwright.browser.logs.ts',
   ],
