f1befc723866eb1465dcde5308d6d9deb7a3901f
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-08-21 14:06:22 +0200

    activity-shapes.ts: order markerPainterFunctions in alphabetic order for consistency

--- src/component/mxgraph/shape/activity-shapes.ts
+++ src/component/mxgraph/shape/activity-shapes.ts
@@ -47,8 +47,8 @@ export abstract class BaseActivityShape extends mxRectangleShape {
   private markerPainterFunctions = new Map<ShapeBpmnMarkerKind, (paintParameter: PaintParameter) => void>([
     [ShapeBpmnMarkerKind.EXPAND, (paintParameter: PaintParameter) => this.iconPainter.paintExpandIcon(paintParameter)],
     [ShapeBpmnMarkerKind.LOOP, (paintParameter: PaintParameter) => this.iconPainter.paintLoopIcon(paintParameter)],
-    [ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, (paintParameter: PaintParameter) => this.iconPainter.paintSequentialMultiInstanceIcon(paintParameter)],
     [ShapeBpmnMarkerKind.MULTI_INSTANCE_PARALLEL, (paintParameter: PaintParameter) => this.iconPainter.paintParallelMultiInstanceIcon(paintParameter)],
+    [ShapeBpmnMarkerKind.MULTI_INSTANCE_SEQUENTIAL, (paintParameter: PaintParameter) => this.iconPainter.paintSequentialMultiInstanceIcon(paintParameter)],
   ]);
 
   constructor() {
