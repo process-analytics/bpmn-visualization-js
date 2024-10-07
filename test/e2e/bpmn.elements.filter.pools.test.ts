e77e13e627cec8a42c6f1f83fff1681ce5249e28
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2024-10-07 16:03:25 +0200

    adjust thresholds for the new Firefox version: filtered

--- test/e2e/bpmn.elements.filter.pools.test.ts
+++ test/e2e/bpmn.elements.filter.pools.test.ts
@@ -32,9 +32,9 @@ describe('Filter pools', () => {
   const diagramSubfolder = 'filter';
   const imageSnapshotConfigurator = new FilterPoolsImageSnapshotConfigurator(
     // chromium: 0.07789950491838837% max
-    // firefox: 0.012128385807519404% max
+    // firefox: 0.024702569803580765% max
     // webkit: 0.160355447672067% max
-    new MultiBrowserImageSnapshotThresholds({ chromium: 0.08 / 100, firefox: 0.013 / 100, webkit: 0.17 / 100 }),
+    new MultiBrowserImageSnapshotThresholds({ chromium: 0.08 / 100, firefox: 0.03 / 100, webkit: 0.17 / 100 }),
     diagramSubfolder,
   );
   const pageTester = new PageTester({ targetedPage: AvailableTestPages.BPMN_RENDERING, diagramSubfolder }, page);
