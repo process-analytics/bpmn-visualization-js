6257ee933954313e82ce1540d641d8e0b2aa58bd
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2026-02-09 15:00:26 +0100

    simplify config and snapshot files reorg

--- test/e2e/bpmn.rendering.ignore.options.test.ts
+++ test/e2e/bpmn.rendering.ignore.options.test.ts
@@ -32,32 +32,13 @@ class ImageSnapshotThresholdsActivityLabelBounds extends MultiBrowserImageSnapsh
   protected override getFirefoxThresholds(): Map<string, ImageSnapshotThresholdConfig> {
     return new Map<string, ImageSnapshotThresholdConfig>([
       [
-        'activities.with.wrongly.positioned.labels',
+        'activities.with.wrongly.positioned.labels.not-ignored',
         {
           linux: 0.23 / 100, // 0.2236111574782207%
         },
       ],
-    ]);
-  }
-
-  protected override getWebkitThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>();
-  }
-}
-
-class ImageSnapshotThresholdsActivityLabelBoundsIgnored extends MultiBrowserImageSnapshotThresholds {
-  constructor() {
-    super({ chromium: 0 / 100, firefox: 0 / 100, webkit: 0 / 100 });
-  }
-
-  protected override getChromiumThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>();
-  }
-
-  protected override getFirefoxThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>([
       [
-        'activities.with.wrongly.positioned.labels',
+        'activities.with.wrongly.positioned.labels.ignored',
         {
           linux: 0.85 / 100, // 0.8457215876566115%
         },
@@ -82,34 +63,15 @@ class ImageSnapshotThresholdsLabelStyles extends MultiBrowserImageSnapshotThresh
   protected override getFirefoxThresholds(): Map<string, ImageSnapshotThresholdConfig> {
     return new Map<string, ImageSnapshotThresholdConfig>([
       [
-        'labels.with.font.styles',
+        'labels.with.font.styles.not-ignored',
         {
-          // very large number because of firefox rendering differences compared to chrome (wrapping, font)
+          // very large number because of firefox rendering differences compared to chrome (font)
           // this test requires to use a dedicated reference screenshot, see https://github.com/process-analytics/bpmn-visualization-js/issues/2838
           linux: 2.4 / 100, // 2.3980839754224936%%
         },
       ],
-    ]);
-  }
-
-  protected override getWebkitThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>();
-  }
-}
-
-class ImageSnapshotThresholdsLabelStylesIgnored extends MultiBrowserImageSnapshotThresholds {
-  constructor() {
-    super({ chromium: 0 / 100, firefox: 0 / 100, webkit: 0 / 100 });
-  }
-
-  protected override getChromiumThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>();
-  }
-
-  protected override getFirefoxThresholds(): Map<string, ImageSnapshotThresholdConfig> {
-    return new Map<string, ImageSnapshotThresholdConfig>([
       [
-        'labels.with.font.styles',
+        'labels.with.font.styles.ignored',
         {
           linux: 0.06 / 100, // 0.05351232277721607%
         },
@@ -126,13 +88,15 @@ describe('BPMN rendering - ignore options', () => {
   const diagramSubfolder = 'bpmn-rendering-ignore-options';
   const pageTester = new PageTester({ targetedPage: AvailableTestPages.BPMN_RENDERING, diagramSubfolder }, page);
 
+  function getConfigName(bpmnDiagramName: string, ignoredOption: boolean): string {
+    return bpmnDiagramName + '.' + (ignoredOption ? 'ignored' : 'not-ignored');
+  }
+
   describe('Ignore activity label bounds', () => {
     const bpmnDiagramName = 'activities.with.wrongly.positioned.labels';
 
     describe.each([false, true])('ignoreBpmnActivityLabelBounds: %s', (ignoreBpmnActivityLabelBounds: boolean) => {
-      const imageSnapshotConfigurator = ignoreBpmnActivityLabelBounds
-        ? new ImageSnapshotConfigurator(new ImageSnapshotThresholdsActivityLabelBoundsIgnored(), 'bpmn-rendering-ignore-options/ignored')
-        : new ImageSnapshotConfigurator(new ImageSnapshotThresholdsActivityLabelBounds(), 'bpmn-rendering-ignore-options/not-ignored');
+      const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new ImageSnapshotThresholdsActivityLabelBounds(), 'bpmn-rendering-ignore-options');
 
       it(`${bpmnDiagramName}`, async () => {
         await pageTester.gotoPageAndLoadBpmnDiagram(bpmnDiagramName, {
@@ -140,7 +104,7 @@ describe('BPMN rendering - ignore options', () => {
         });
 
         const image = await page.screenshot({ fullPage: true });
-        const config = imageSnapshotConfigurator.getConfig(bpmnDiagramName);
+        const config = imageSnapshotConfigurator.getConfig(getConfigName(bpmnDiagramName, ignoreBpmnActivityLabelBounds));
         expect(image).toMatchImageSnapshot(config);
       });
     });
@@ -150,9 +114,7 @@ describe('BPMN rendering - ignore options', () => {
     const bpmnDiagramName = 'labels.with.font.styles';
 
     describe.each([false, true])('ignoreBpmnLabelStyles: %s', (ignoreBpmnLabelStyles: boolean) => {
-      const imageSnapshotConfigurator = ignoreBpmnLabelStyles
-        ? new ImageSnapshotConfigurator(new ImageSnapshotThresholdsLabelStylesIgnored(), 'bpmn-rendering-ignore-options/ignored')
-        : new ImageSnapshotConfigurator(new ImageSnapshotThresholdsLabelStyles(), 'bpmn-rendering-ignore-options/not-ignored');
+      const imageSnapshotConfigurator = new ImageSnapshotConfigurator(new ImageSnapshotThresholdsLabelStyles(), 'bpmn-rendering-ignore-options');
 
       it(`${bpmnDiagramName}`, async () => {
         await pageTester.gotoPageAndLoadBpmnDiagram(bpmnDiagramName, {
@@ -160,7 +122,7 @@ describe('BPMN rendering - ignore options', () => {
         });
 
         const image = await page.screenshot({ fullPage: true });
-        const config = imageSnapshotConfigurator.getConfig(bpmnDiagramName);
+        const config = imageSnapshotConfigurator.getConfig(getConfigName(bpmnDiagramName, ignoreBpmnLabelStyles));
         expect(image).toMatchImageSnapshot(config);
       });
     });
